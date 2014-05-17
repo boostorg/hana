44138a1a531803c2ad1bcd09e3bf4fdc204c9933
Louis Dionne <ldionne.2@gmail.com>
2014-05-17 17:13:55 -0600

    Add type traits to Type.

--- include/boost/hana/type.hpp
+++ include/boost/hana/type.hpp
@@ -14,11 +14,13 @@
 
 #include <boost/hana/integral.hpp>
 
+#include <type_traits>
+
 
 namespace boost { namespace hana {
     //! @ingroup datatypes
     template <typename T>
-    struct Type { };
+    struct Type { using type = T; };
 
     template <typename T>
     constexpr Type<T> type{};
@@ -31,6 +33,207 @@ namespace boost { namespace hana {
     template <typename T, typename U>
     constexpr auto operator!=(Type<T> t, Type<U> u) -> decltype(!(t == u))
     { return !(t == u); }
+
+#define BOOST_HANA_WRAP_TYPE_TRAIT(trait)                                   \
+    constexpr struct _ ## trait {                                           \
+        template <typename T>                                               \
+        constexpr Type<typename std::trait<T>::type>                        \
+        operator()(Type<T>) const                                           \
+        { return {}; }                                                      \
+    } trait{};                                                              \
+/**/
+
+#define BOOST_HANA_BOOLEAN_TYPE_TRAIT(trait)                                \
+    constexpr struct _ ## trait {                                           \
+        template <typename T>                                               \
+        constexpr Bool<std::trait<T>::value>                                \
+        operator()(Type<T>) const                                           \
+        { return {}; }                                                      \
+    } trait{};                                                              \
+/**/
+
+#define BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT(trait)                         \
+    constexpr struct _ ## trait {                                           \
+        template <typename T, typename U>                                   \
+        constexpr Bool<std::trait<T, U>::value>                             \
+        operator()(Type<T>, Type<U>) const                                  \
+        { return {}; }                                                      \
+    } trait{};                                                              \
+/**/
+
+#define BOOST_HANA_BOOLEAN_NARY_TYPE_TRAIT(trait)                           \
+    constexpr struct _ ## trait {                                           \
+        template <typename T, typename ...U>                                \
+        constexpr Bool<std::trait<T, U...>::value>                          \
+        operator()(Type<T>, Type<U>...) const                               \
+        { return {}; }                                                      \
+    } trait{};                                                              \
+/**/
+
+#define BOOST_HANA_INTEGRAL_TYPE_TRAIT(trait)                               \
+    constexpr struct _ ## trait {                                           \
+        template <typename T>                                               \
+        constexpr Integral<                                                 \
+            decltype(std::trait<T>::value), std::trait<T>::value            \
+        > operator()(Type<T>) const                                         \
+        { return {}; }                                                      \
+    } trait{};                                                              \
+/**/
+
+    namespace trait {
+        ///////////////////
+        // Type properties
+        ///////////////////
+        // Primary type categories
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_void)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_null_pointer)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_integral)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_floating_point)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_array)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_enum)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_union)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_class)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_function)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_pointer)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_lvalue_reference)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_rvalue_reference)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_member_object_pointer)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_member_function_pointer)
+
+        // Composite type categories
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_fundamental)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_arithmetic)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_scalar)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_object)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_compound)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_reference)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_member_pointer)
+
+        // Type properties
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_const)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_volatile)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivial)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivially_copyable)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_standard_layout)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_pod)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_literal_type)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_empty)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_polymorphic)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_abstract)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_signed)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_unsigned)
+
+        // Supported operations
+        BOOST_HANA_BOOLEAN_NARY_TYPE_TRAIT(is_constructible)
+        BOOST_HANA_BOOLEAN_NARY_TYPE_TRAIT(is_trivially_constructible)
+        BOOST_HANA_BOOLEAN_NARY_TYPE_TRAIT(is_nothrow_constructible)
+
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_default_constructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivially_default_constructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_nothrow_default_constructible)
+
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_copy_constructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivially_copy_constructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_nothrow_copy_constructible)
+
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_move_constructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivially_move_constructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_nothrow_move_constructible)
+
+        BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT(is_assignable)
+        BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT(is_trivially_assignable)
+        BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT(is_nothrow_assignable)
+
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_copy_assignable)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivially_copy_assignable)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_nothrow_copy_assignable)
+
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_move_assignable)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivially_move_assignable)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_nothrow_move_assignable)
+
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_destructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_trivially_destructible)
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(is_nothrow_destructible)
+
+        BOOST_HANA_BOOLEAN_TYPE_TRAIT(has_virtual_destructor)
+
+        // Property queries
+        BOOST_HANA_INTEGRAL_TYPE_TRAIT(alignment_of)
+        BOOST_HANA_INTEGRAL_TYPE_TRAIT(rank)
+        BOOST_HANA_INTEGRAL_TYPE_TRAIT(extent)
+
+        // Type relationships
+        BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT(is_same)
+        BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT(is_base_of)
+        BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT(is_convertible)
+
+        ///////////////////
+        // Type modifications
+        ///////////////////
+        // Const-volatility specifiers
+        BOOST_HANA_WRAP_TYPE_TRAIT(remove_cv)
+        BOOST_HANA_WRAP_TYPE_TRAIT(remove_const)
+        BOOST_HANA_WRAP_TYPE_TRAIT(remove_volatile)
+
+        BOOST_HANA_WRAP_TYPE_TRAIT(add_cv)
+        BOOST_HANA_WRAP_TYPE_TRAIT(add_const)
+        BOOST_HANA_WRAP_TYPE_TRAIT(add_volatile)
+
+        // References
+        BOOST_HANA_WRAP_TYPE_TRAIT(remove_reference)
+        BOOST_HANA_WRAP_TYPE_TRAIT(add_lvalue_reference)
+        BOOST_HANA_WRAP_TYPE_TRAIT(add_rvalue_reference)
+
+        // Pointers
+        BOOST_HANA_WRAP_TYPE_TRAIT(remove_pointer)
+        BOOST_HANA_WRAP_TYPE_TRAIT(add_pointer)
+
+        // Sign modifiers
+        BOOST_HANA_WRAP_TYPE_TRAIT(make_signed)
+        BOOST_HANA_WRAP_TYPE_TRAIT(make_unsigned)
+
+        // Arrays
+        BOOST_HANA_WRAP_TYPE_TRAIT(remove_extent)
+        BOOST_HANA_WRAP_TYPE_TRAIT(remove_all_extents)
+
+        // Miscellaneous transformations
+        constexpr struct _aligned_storage {
+            template <std::size_t Len, std::size_t Align>
+            constexpr typename std::aligned_storage<Len, Align>::type
+            operator()(SizeT<Len>, SizeT<Align>) const { return {}; }
+
+            template <std::size_t Len>
+            constexpr typename std::aligned_storage<Len>::type
+            operator()(SizeT<Len>) const { return {}; }
+        } aligned_storage{};
+
+        constexpr struct _aligned_union {
+            template <std::size_t Len, typename ...T>
+            constexpr typename std::aligned_union<Len, T...>::type
+            operator()(SizeT<Len>, Type<T>...) const { return {}; }
+        } aligned_union{};
+
+        BOOST_HANA_WRAP_TYPE_TRAIT(decay)
+        // enable_if
+        // disable_if
+        // conditional
+
+        constexpr struct _common_type {
+            template <typename ...T>
+            constexpr Type<typename std::common_type<T...>::type>
+            operator()(Type<T>...) const { return {}; }
+        } common_type{};
+
+        BOOST_HANA_WRAP_TYPE_TRAIT(underlying_type)
+        BOOST_HANA_WRAP_TYPE_TRAIT(result_of)
+    } // end namespace trait
+
+#undef BOOST_HANA_WRAP_TYPE_TRAIT
+#undef BOOST_HANA_BOOLEAN_TYPE_TRAIT
+#undef BOOST_HANA_BOOLEAN_BINARY_TYPE_TRAIT
+#undef BOOST_HANA_BOOLEAN_NARY_TYPE_TRAIT
+#undef BOOST_HANA_INTEGRAL_TYPE_TRAIT
 }} // end namespace boost::hana
 
 #endif // !BOOST_HANA_TYPE_HPP
--- test/type.cpp
+++ test/type.cpp
@@ -14,7 +14,144 @@ using namespace boost::hana;
 template <bool b = false>
 struct invalid { static_assert(b, "invalid must not be instantiated"); };
 
+void test_type_traits() {
+    enum _e { };          constexpr auto e = type<_e>;
+    struct _s { };        constexpr auto s = type<_s>;
+    using _f = void(*)();
+
+    // We just make sure that they compile. If the forwarding to `std::` is
+    // well done, it is the job of `std::` to return the right thing.
+
+    ///////////////////
+    // Type properties
+    ///////////////////
+    // Primary type categories
+    trait::is_void(s);
+    trait::is_null_pointer(s);
+    trait::is_integral(s);
+    trait::is_floating_point(s);
+    trait::is_array(s);
+    trait::is_enum(s);
+    trait::is_union(s);
+    trait::is_class(s);
+    trait::is_function(s);
+    trait::is_pointer(s);
+    trait::is_lvalue_reference(s);
+    trait::is_rvalue_reference(s);
+    trait::is_member_object_pointer(s);
+    trait::is_member_function_pointer(s);
+
+    // Composite type categories
+    trait::is_fundamental(s);
+    trait::is_arithmetic(s);
+    trait::is_scalar(s);
+    trait::is_object(s);
+    trait::is_compound(s);
+    trait::is_reference(s);
+    trait::is_member_pointer(s);
+
+    // Type properties
+    trait::is_const(s);
+    trait::is_volatile(s);
+    trait::is_trivial(s);
+    trait::is_trivially_copyable(s);
+    trait::is_standard_layout(s);
+    trait::is_pod(s);
+    trait::is_literal_type(s);
+    trait::is_empty(s);
+    trait::is_polymorphic(s);
+    trait::is_abstract(s);
+    trait::is_signed(s);
+    trait::is_unsigned(s);
+
+    // Supported operations
+    trait::is_constructible(s, s);
+    trait::is_trivially_constructible(s, s);
+    trait::is_nothrow_constructible(s, s);
+
+    trait::is_default_constructible(s);
+    trait::is_trivially_default_constructible(s);
+    trait::is_nothrow_default_constructible(s);
+
+    trait::is_copy_constructible(s);
+    trait::is_trivially_copy_constructible(s);
+    trait::is_nothrow_copy_constructible(s);
+
+    trait::is_move_constructible(s);
+    trait::is_trivially_move_constructible(s);
+    trait::is_nothrow_move_constructible(s);
+
+    trait::is_assignable(s, s);
+    trait::is_trivially_assignable(s, s);
+    trait::is_nothrow_assignable(s, s);
+
+    trait::is_copy_assignable(s);
+    trait::is_trivially_copy_assignable(s);
+    trait::is_nothrow_copy_assignable(s);
+
+    trait::is_move_assignable(s);
+    trait::is_trivially_move_assignable(s);
+    trait::is_nothrow_move_assignable(s);
+
+    trait::is_destructible(s);
+    trait::is_trivially_destructible(s);
+    trait::is_nothrow_destructible(s);
+
+    trait::has_virtual_destructor(s);
+
+    // Property queries
+    trait::alignment_of(s);
+    trait::rank(s);
+    trait::extent(s);
+
+    // Type relationships
+    trait::is_same(s, s);
+    trait::is_base_of(s, s);
+    trait::is_convertible(s, s);
+
+    ///////////////////
+    // Type modifications
+    ///////////////////
+    // Const-volatility specifiers
+    trait::remove_cv(s);
+    trait::remove_const(s);
+    trait::remove_volatile(s);
+
+    trait::add_cv(s);
+    trait::add_const(s);
+    trait::add_volatile(s);
+
+    // References
+    trait::remove_reference(s);
+    trait::add_lvalue_reference(s);
+    trait::add_rvalue_reference(s);
+
+    // Pointers
+    trait::remove_pointer(s);
+    trait::add_pointer(s);
+
+    // Sign modifiers
+    trait::make_signed(type<unsigned>);
+    trait::make_unsigned(type<signed>);
+
+    // Arrays
+    trait::remove_extent(s);
+    trait::remove_all_extents(s);
+
+    // Miscellaneous transformations
+    trait::aligned_storage(size_t<1>);
+    trait::aligned_storage(size_t<1>, size_t<1>);
+    trait::aligned_union(size_t<0>, s);
+    trait::decay(s);
+
+    trait::common_type(s, s);
+    trait::underlying_type(e);
+    trait::result_of(type<_f(void)>);
+}
+
 int main() {
+    test_type_traits();
+
     BOOST_HANA_STATIC_ASSERT(type<void> == type<void>);
     BOOST_HANA_STATIC_ASSERT(type<void> != type<int>);
 
