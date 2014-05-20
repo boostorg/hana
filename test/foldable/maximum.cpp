e9d396f79d7a5dfd655662d6eb15e235f2ba201e
Louis Dionne <ldionne.2@gmail.com>
2014-05-20 18:46:56 -0400

    Split Foldable unit tests.

---
+++ test/foldable/maximum.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright Louis Dionne 2014
+ * Distributed under the Boost Software License, Version 1.0.
+ *         (See accompanying file LICENSE.md or copy at
+ *             http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <boost/hana/foldable.hpp>
+
+#include <boost/hana/detail/static_assert.hpp>
+#include <boost/hana/integral.hpp>
+
+#include "minimal_foldable.hpp"
+using namespace boost::hana;
+
+
+int main() {
+    BOOST_HANA_STATIC_ASSERT(maximum(foldable(int_<0>)) == int_<0>);
+    BOOST_HANA_STATIC_ASSERT(maximum(foldable(int_<0>, int_<1>)) == int_<1>);
+    BOOST_HANA_STATIC_ASSERT(maximum(foldable(int_<1>, int_<0>)) == int_<1>);
+}
---
+++ test/foldable/minimal_foldable.hpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright Louis Dionne 2014
+ * Distributed under the Boost Software License, Version 1.0.
+ *         (See accompanying file LICENSE.md or copy at
+ *             http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef BOOST_HANA_TEST_FOLDABLE_MINIMAL_FOLDABLE_HPP
+#define BOOST_HANA_TEST_FOLDABLE_MINIMAL_FOLDABLE_HPP
+
+#include <boost/hana/detail/constexpr.hpp>
+#include <boost/hana/list.hpp>
+
+
+//! @todo Write a real minimal foldable.
+BOOST_HANA_CONSTEXPR_LAMBDA auto foldable = boost::hana::list;
+
+#endif // !BOOST_HANA_TEST_FOLDABLE_MINIMAL_FOLDABLE_HPP
