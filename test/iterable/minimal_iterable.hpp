d806fb1e8cddb0d4547355c3e25c9dc7e2755f90
Louis Dionne <ldionne.2@gmail.com>
2014-05-20 18:40:42 -0400

    Split Iterable unit tests.

---
+++ test/iterable/minimal_iterable.hpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright Louis Dionne 2014
+ * Distributed under the Boost Software License, Version 1.0.
+ *         (See accompanying file LICENSE.md or copy at
+ *             http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef BOOST_HANA_TEST_ITERABLE_MINIMAL_ITERABLE_HPP
+#define BOOST_HANA_TEST_ITERABLE_MINIMAL_ITERABLE_HPP
+
+#include <boost/hana/detail/constexpr.hpp>
+#include <boost/hana/list.hpp>
+
+
+//! @todo Write a real minimal iterable.
+BOOST_HANA_CONSTEXPR_LAMBDA auto iterable = boost::hana::list;
+
+#endif // !BOOST_HANA_TEST_ITERABLE_MINIMAL_ITERABLE_HPP
