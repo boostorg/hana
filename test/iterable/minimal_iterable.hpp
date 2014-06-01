49c4417626401b911a3467c5044eb7993ca9d627
Louis Dionne <ldionne.2@gmail.com>
2014-06-01 10:35:19 -0400

    Add a genuine minimal Iterable.

--- test/iterable/minimal_iterable.hpp
+++ test/iterable/minimal_iterable.hpp
@@ -8,11 +8,59 @@
 #ifndef BOOST_HANA_TEST_ITERABLE_MINIMAL_ITERABLE_HPP
 #define BOOST_HANA_TEST_ITERABLE_MINIMAL_ITERABLE_HPP
 
+#include <boost/hana/comparable.hpp>
+#include <boost/hana/detail/comparable_from_iterable.hpp>
 #include <boost/hana/detail/constexpr.hpp>
+#include <boost/hana/detail/foldable_from_iterable.hpp>
+#include <boost/hana/foldable.hpp>
+#include <boost/hana/iterable.hpp>
 #include <boost/hana/list.hpp>
 
 
-//! @todo Write a real minimal iterable.
-BOOST_HANA_CONSTEXPR_LAMBDA auto iterable = boost::hana::list;
+namespace {
+    template <typename T, typename Datatype>
+    struct with_datatype_impl {
+        T actual;
+        using hana_datatype = Datatype;
+    };
+
+    struct MinimalIterable;
+
+    BOOST_HANA_CONSTEXPR_LAMBDA auto iterable = [](auto ...xs) {
+        return with_datatype_impl<
+            decltype(boost::hana::list(xs...)), MinimalIterable
+        >{boost::hana::list(xs...)};
+    };
+}
+
+namespace boost { namespace hana {
+    template <>
+    struct Iterable<MinimalIterable> : defaults<Iterable> {
+        template <typename Xs>
+        static constexpr auto head_impl(Xs xs)
+        { return head(xs.actual); }
+
+        template <typename Xs>
+        static constexpr auto tail_impl(Xs xs) {
+            return with_datatype_impl<
+                decltype(tail(xs.actual)), MinimalIterable
+            >{tail(xs.actual)};
+        }
+
+        template <typename Xs>
+        static constexpr auto is_empty_impl(Xs xs)
+        { return is_empty(xs.actual); }
+    };
+
+    template <>
+    struct Foldable<MinimalIterable>
+        : detail::foldable_from_iterable
+    { };
+
+    template <>
+    struct Comparable<MinimalIterable, MinimalIterable>
+        : detail::comparable_from_iterable
+    { };
+}}
 
 #endif // !BOOST_HANA_TEST_ITERABLE_MINIMAL_ITERABLE_HPP
