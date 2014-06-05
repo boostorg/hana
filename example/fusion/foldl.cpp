8fcf0c21ddae0387c5f7c1806122bc80612ff9fc
Louis Dionne <ldionne.2@gmail.com>
2014-06-04 21:16:18 -0400

    Examples: use sstream instead of lexical_cast in folds (reduce compile-time)

--- example/fusion/foldl.cpp
+++ example/fusion/foldl.cpp
@@ -4,17 +4,17 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-#include <boost/hana/detail/constexpr.hpp>
 #include <boost/hana/list.hpp>
 
-#include <boost/lexical_cast.hpp>
 #include <cassert>
+#include <sstream>
 #include <string>
 using namespace boost::hana;
 
 
-BOOST_HANA_CONSTEXPR_LAMBDA auto make_string = [](std::string str, auto x)
-{ return str + boost::lexical_cast<std::string>(x); };
+auto make_string = [](std::string str, auto x) {
+    return str + (std::ostringstream{} << x).str();
+};
 
 int main() {
     assert(foldl(make_string, "1", list(2, "3", '4')) == "1234");
--- example/fusion/foldr.cpp
+++ example/fusion/foldr.cpp
@@ -4,18 +4,18 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-#include <boost/hana/detail/constexpr.hpp>
 #include <boost/hana/list.hpp>
 
-#include <boost/lexical_cast.hpp>
 #include <cassert>
+#include <sstream>
 #include <string>
 using namespace boost::hana;
 
 
-BOOST_HANA_CONSTEXPR_LAMBDA auto make_string = [](auto x, std::string str)
-{ return boost::lexical_cast<std::string>(x) + str; };
+auto make_string = [](auto x, std::string str) {
+    return str + (std::ostringstream{} << x).str();
+};
 
 int main() {
-    assert(foldr(make_string, "4", list(1, "2", '3')) == "1234");
+    assert(foldr(make_string, "4", list(1, "2", '3')) == "4321");
 }
