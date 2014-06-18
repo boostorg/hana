cd37845ce2913fbd297bab6b3e525a4b69335b19
Louis Dionne <ldionne.2@gmail.com>
2014-06-18 14:46:45 -0400

    Range: convert Integrals to their underlying type explicitly

--- include/boost/hana/range.hpp
+++ include/boost/hana/range.hpp
@@ -69,7 +69,7 @@ namespace boost { namespace hana {
         // variable whose type is dependent so we can use `valid_range` as a
         // constant expression below.
         auto valid_range = from <= to;
-        static_assert(valid_range,
+        static_assert(valid_range(),
         "invalid usage of boost::hana::range(from, to) with from > to");
         return operators::_range<decltype(from), decltype(to)>{from, to};
     };
@@ -110,7 +110,7 @@ namespace boost { namespace hana {
     struct Foldable<Range> : instance<Foldable>::with<Range> {
         template <typename F, typename From, typename T, T ...vs>
         static constexpr auto unpack_helper(F f, From from, detail::integer_sequence<T, vs...>)
-        { return f(integral<T, from + vs>...); }
+        { return f(integral<T, from() + vs>...); }
 
         template <typename F, typename R>
         static constexpr auto unpack_impl(F f, R r) {
@@ -118,7 +118,7 @@ namespace boost { namespace hana {
             // variable whose type is dependent so we can use `size` as a
             // constant expression below.
             auto size = r.to - r.from;
-            return unpack_helper(f, r.from, detail::make_integer_sequence<decltype(r.from()), size>{});
+            return unpack_helper(f, r.from, detail::make_integer_sequence<decltype(r.from()), size()>{});
         }
 
         template <typename R>
@@ -130,9 +130,7 @@ namespace boost { namespace hana {
     constexpr bool comparable_from_iterable<Range> = true;
 
     template <>
-    struct Comparable<Range, Range>
-        : defaults<Comparable>::template with<Range, Range>
-    {
+    struct Comparable<Range, Range> : defaults<Comparable>::with<Range, Range> {
         template <typename R1, typename R2>
         static constexpr auto equal_impl(R1 r1, R2 r2) {
             return (is_empty(r1) && is_empty(r2)) ||
