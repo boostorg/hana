6aa9c3e3c812c73c2b50269628566545ecb30e1c
Louis Dionne <ldionne.2@gmail.com>
2014-07-08 10:17:33 -0400

    Add tests for Fusion tuple.

--- test/ext/boost/fusion/helper.hpp
+++ test/ext/boost/fusion/helper.hpp
@@ -14,12 +14,14 @@ Distributed under the Boost Software License, Version 1.0.
 #include <boost/fusion/container/generation/make_list.hpp>
 #include <boost/fusion/container/generation/make_vector.hpp>
 #include <boost/fusion/container/list/cons.hpp>
+#include <boost/fusion/tuple/make_tuple.hpp>
 
 
 auto with_nonassociative_forward_sequences = [](auto test) {
     test([](auto ...xs) { return boost::fusion::make_vector(xs...); });
     test([](auto ...xs) { return boost::fusion::make_list(xs...); });
     test([](auto ...xs) { return boost::fusion::make_deque(xs...); });
+    test([](auto ...xs) { return boost::fusion::make_tuple(xs...); });
     test([](auto ...xs) {
         return boost::hana::foldr(
             [](auto x, auto xs) { return boost::fusion::make_cons(x, xs); },
