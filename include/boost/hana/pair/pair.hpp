83dca441bb56145cc4ff4e97144570529424d2ec
Louis Dionne <ldionne.2@gmail.com>
2014-07-23 16:13:40 -0400

    Typeclasses: move provided instances for non-builtin types out of the forward decls.

--- include/boost/hana/pair/pair.hpp
+++ include/boost/hana/pair/pair.hpp
@@ -10,10 +10,8 @@ Distributed under the Boost Software License, Version 1.0.
 #ifndef BOOST_HANA_PAIR_PAIR_HPP
 #define BOOST_HANA_PAIR_PAIR_HPP
 
-#include <boost/hana/comparable/equal_mcd.hpp>
-#include <boost/hana/core.hpp> // for instantiates
 #include <boost/hana/detail/constexpr.hpp>
-#include <boost/hana/logical/logical.hpp>
+#include <boost/hana/detail/typeclasses.hpp>
 
 
 namespace boost { namespace hana {
@@ -56,80 +54,6 @@ namespace boost { namespace hana {
             datatype_t<decltype(pair)>
         >::second_impl(pair);
     };
-
-    //! @details
-    //! Two pairs `x` and `y` are equal iff they are equal element-wise,
-    //! i.e. iff
-    //! @code
-    //!     first(x) == first(y) && second(x) == second(y)
-    //! @endcode
-    //!
-    //! ### Example
-    //! @snippet example/pair/comparable.cpp main
-    template <typename T, typename U>
-    struct Comparable::instance<T, U, when<
-        instantiates<Pair, T>() && instantiates<Pair, U>()
-    >> : Comparable::equal_mcd {
-        template <typename X, typename Y>
-        static constexpr auto equal_impl(X x, Y y) {
-            return and_(
-                equal(first(x), first(y)),
-                equal(second(x), second(y))
-            );
-        }
-    };
-
-#if 0
-    template <typename T>
-    struct Functor::instance<T, when<instantiates<Pair, T>()>>
-        : Functor::fmap_mcd
-    {
-        template <typename F, typename P>
-        static constexpr auto fmap_impl(F f, P p)
-        { return pair(f(first(p)), f(second(p))); }
-    };
-
-    template <typename T>
-    struct Applicative::instance<T, when<instantiates<Pair, T>()>>
-        : Applicative::mcd
-    {
-        template <typename X>
-        static constexpr auto lift_impl(X x)
-        { return pair(x, x); }
-
-        template <typename F, typename P>
-        static constexpr auto ap_impl(F f, P p)
-        { return pair(first(f)(first(p), second(f)(second(p)))); }
-    };
-
-    template <typename T>
-    struct Monad::instance<T, when<instantiates<Pair, T>()>>
-        : Monad::bind_mcd
-    {
-        template <typename P, typename F>
-        static constexpr auto bind_impl(P p, F f) {
-            return pair(
-                bind(first(p), compose(first, f)),
-                bind(second(p), compose(second, f))
-            );
-        }
-    };
-
-    template <typename T>
-    struct Foldable::instance<T, when<instantiates<Pair, T>()>>
-        : Foldable::mcd
-    {
-        template <typename F, typename S, typename P>
-        static constexpr auto foldr_impl(F f, S s, P p) {
-            return f(first(p), f(second(p), s));
-        }
-
-        template <typename F, typename S, typename P>
-        static constexpr auto foldl_impl(F f, S s, P p) {
-            return f(f(s, first(p)), second(p));
-        }
-    };
-#endif
 }} // end namespace boost::hana
 
 #endif // !BOOST_HANA_PAIR_PAIR_HPP
--- include/boost/hana/record/mcd.hpp
+++ include/boost/hana/record/mcd.hpp
@@ -12,11 +12,88 @@ Distributed under the Boost Software License, Version 1.0.
 
 #include <boost/hana/record/record.hpp>
 
+#include <boost/hana/comparable/equal_mcd.hpp>
+#include <boost/hana/core.hpp> // for instantiates
+#include <boost/hana/detail/std/type_traits.hpp>
+#include <boost/hana/foldable/mcd.hpp>
+#include <boost/hana/map.hpp>
+#include <boost/hana/pair/instance.hpp>
+#include <boost/hana/searchable/find_mcd.hpp>
+
 
 namespace boost { namespace hana {
     //! Minimal complete definition: `members`
-    struct Record::mcd {
+    struct Record::mcd { };
 
+    //! Two `Records` of the same data type `R` are equal if and only if
+    //! all their members are equal. The members are compared in the
+    //! same order as they appear in `members<R>`.
+    template <typename R>
+    struct Comparable::instance<R, R, when<instantiates<Record, R>()>>
+        : Comparable::equal_mcd
+    {
+        template <typename X, typename Y>
+        static constexpr auto equal_impl(X x, Y y) {
+            return all([=](auto k_f) {
+                return equal(second(k_f)(x), second(k_f)(y));
+            }, members<R>);
+        }
+    };
+
+    //! Folding a `Record` `R` is equivalent to folding a list of its members,
+    //! in the same order as they appear in `members<R>`.
+    template <typename R>
+    struct Foldable::instance<R, when<instantiates<Record, R>()>>
+        : Foldable::mcd
+    {
+        template <typename F, typename S, typename X>
+        static constexpr auto foldl_impl(F f, S s, X x) {
+            auto g = [=](auto s, auto k_f) {
+                return f(s, second(k_f)(x));
+            };
+            return foldl(g, s, members<R>);
+        }
+
+        template <typename F, typename S, typename X>
+        static constexpr auto foldr_impl(F f, S s, X x) {
+            auto g = [=](auto k_f, auto s) {
+                return f(second(k_f)(x), s);
+            };
+            return foldr(g, s, members<R>);
+        }
+    };
+
+    //! Searching a `Record` `r` is equivalent to searching `to<Map>(r)`.
+    template <typename R>
+    struct Searchable::instance<R, when<instantiates<Record, R>()>>
+        : Searchable::find_mcd
+    {
+        template <typename Pred, typename X>
+        static constexpr auto find_impl(Pred pred, X x) {
+            return fmap(
+                [=](auto k_f) { return second(k_f)(x); },
+                find([=](auto k_f) { return pred(first(k_f)); }, members<R>)
+            );
+        }
+
+        template <typename Pred, typename X>
+        static constexpr auto any_impl(Pred pred, X x) {
+            return any([=](auto k_f) { return pred(first(k_f)); }, members<R>);
+        }
+    };
+
+    //! Converting a `Record` `R` to a `Map` is equivalent to converting its
+    //! `members<R>` to a `Map`, except the values are replaced by the actual
+    //! members of the object instead of accessors.
+    template <typename R>
+    struct convert<Map, R, detail::std::enable_if_t<instantiates<Record, R>()>> {
+        template <typename X>
+        static constexpr auto apply(X x) {
+            return to<Map>(fmap(
+                [=](auto k_f) { return pair(first(k_f), second(k_f)(x)); },
+                members<R>
+            ));
+        }
     };
 }} // end namespace boost::hana
 
