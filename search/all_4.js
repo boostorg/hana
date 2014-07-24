342ffb28900b5dff992829e2fca1b9c7d8406115
Louis Dionne <ldionne.2@gmail.com>
2014-07-23 20:47:12 -0400

    Update to c42daba

--- search/all_4.js
+++ search/all_4.js
@@ -4,6 +4,7 @@ var searchData=
   ['datatype_5ft',['datatype_t',['../group__core.html#gae3cedd3772d00ac7c4de9dc3d719f321',1,'boost::hana']]],
   ['data_20types',['Data types',['../group__datatypes.html',1,'']]],
   ['decltype_5f',['decltype_',['../structboost_1_1hana_1_1_type.html#a81f30da3c8d927156d17ec09b8a2a4de',1,'boost::hana::Type']]],
+  ['default_5finstance',['default_instance',['../structboost_1_1hana_1_1_comparable_1_1default__instance.html',1,'boost::hana::Comparable']]],
   ['demux',['demux',['../group___functional.html#ga3a8316acd5efa22c3d1861b62d5df3c2',1,'boost::hana']]],
   ['dependent_5fon_2ehpp',['dependent_on.hpp',['../dependent__on_8hpp.html',1,'']]],
   ['details',['Details',['../group__details.html',1,'']]],
