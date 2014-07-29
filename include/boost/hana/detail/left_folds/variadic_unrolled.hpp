4fd8b276357439600dfcf79a39319d3779debf90
Louis Dionne <ldionne.2@gmail.com>
2014-07-28 22:35:27 -0400

    Detail: Add missing constexpr in variadic_unrolled left fold

--- include/boost/hana/detail/left_folds/variadic_unrolled.hpp
+++ include/boost/hana/detail/left_folds/variadic_unrolled.hpp
@@ -15,31 +15,31 @@ Distributed under the Boost Software License, Version 1.0.
 
 namespace boost { namespace hana { namespace detail { namespace left_folds {
     template <typename F, typename State>
-    auto variadic_unrolled_impl(F, State s)
+    constexpr auto variadic_unrolled_impl(F, State s)
     { return s; }
 
     template <typename F, typename State, typename X0>
-    auto variadic_unrolled_impl(F f, State s, X0 x0)
+    constexpr auto variadic_unrolled_impl(F f, State s, X0 x0)
     { return f(s, x0); }
 
     template <typename F, typename State, typename X0, typename X1>
-    auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1)
+    constexpr auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1)
     { return f(f(s, x0), x1); }
 
     template <typename F, typename State, typename X0, typename X1, typename X2>
-    auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2)
+    constexpr auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2)
     { return f(f(f(s, x0), x1), x2); }
 
     template <typename F, typename State, typename X0, typename X1, typename X2, typename X3>
-    auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2, X3 x3)
+    constexpr auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2, X3 x3)
     { return f(f(f(f(s, x0), x1), x2), x3); }
 
     template <typename F, typename State, typename X0, typename X1, typename X2, typename X3, typename X4>
-    auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2, X3 x3, X4 x4)
+    constexpr auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2, X3 x3, X4 x4)
     { return f(f(f(f(f(s, x0), x1), x2), x3), x4); }
 
     template <typename F, typename State, typename X0, typename X1, typename X2, typename X3, typename X4, typename X5, typename ...Xs>
-    auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2, X3 x3, X4 x4, X5 x5, Xs ...xs)
+    constexpr auto variadic_unrolled_impl(F f, State s, X0 x0, X1 x1, X2 x2, X3 x3, X4 x4, X5 x5, Xs ...xs)
     { return variadic_unrolled_impl(f, f(f(f(f(f(f(s, x0), x1), x2), x3), x4), x5), xs...); }
 
     BOOST_HANA_CONSTEXPR_LAMBDA auto variadic_unrolled = [](auto f, auto s, auto ...xs) {
