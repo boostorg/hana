8f593765a854e909fbf157941a6844bc8d0c904c
Louis Dionne <ldionne.2@gmail.com>
2014-08-30 14:38:21 -0400

    Set: improve Comparable example

--- example/set.cpp
+++ example/set.cpp
@@ -6,17 +6,22 @@ Distributed under the Boost Software License, Version 1.0.
 
 #include <boost/hana/detail/assert.hpp>
 #include <boost/hana/detail/constexpr.hpp>
+#include <boost/hana/foreign.hpp>
 #include <boost/hana/integral.hpp>
 #include <boost/hana/maybe.hpp>
 #include <boost/hana/set.hpp>
+#include <boost/hana/type.hpp>
 using namespace boost::hana;
 
 
 int main() {
     //! [comparable]
     BOOST_HANA_CONSTANT_ASSERT(
-        set(int_<0>, int_<1>, int_<2>) == set(int_<2>, int_<1>, int_<0>)
+        set(int_<0>, type<char>, int_<1>) == set(int_<1>, int_<0>, type<char>)
     );
+
+    BOOST_HANA_CONSTEXPR_ASSERT(set(1, '2', 3.3) == set('2', 1, 3.3));
+    BOOST_HANA_CONSTANT_ASSERT(set(1, '2', 3.3) != set('2', 1));
     //! [comparable]
 
     //! [searchable]
