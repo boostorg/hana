fd473470c34b8e41d58dcf770265c7c4d139c1c2
Louis Dionne <ldionne.2@gmail.com>
2014-09-30 08:21:36 -0400

    Add two runtime benchmarks for foldl

---
+++ benchmark/vs/foldl/hana.cpp
@@ -0,0 +1,29 @@
+/*
+@copyright Zach Laine 2014
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+ */
+
+#include <boost/hana/tuple/foldable.hpp>
+
+#include "benchmark.hpp"
+#include <cstdlib>
+namespace bh = boost::hana;
+
+
+int main () {
+    boost::hana::benchmark::measure([] {
+        long double result = 0;
+        for (int iteration = 0; iteration < 1 << 10; ++iteration) {
+            auto values = bh::tuple(
+                <%=
+                    types.map { |t| "static_cast<#{t}>(std::rand())" }.join(', ')
+                %>
+            );
+
+            result += bh::foldl(values, 0, [](auto state, auto t) {
+                return state + t;
+            });
+        }
+    });
+}
