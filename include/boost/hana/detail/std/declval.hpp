187f396711b0bc3add41b19dcd87503d3e8df667
Louis Dionne <ldionne.2@gmail.com>
2014-10-25 12:56:30 -0400

    Add std::declval and std::add_rvalue_reference to detail::

---
+++ include/boost/hana/detail/std/declval.hpp
@@ -0,0 +1,21 @@
+/*!
+@file
+Defines an equivalent to `std::declval`.
+
+@copyright Louis Dionne 2014
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef BOOST_HANA_DETAIL_STD_DECLVAL_HPP
+#define BOOST_HANA_DETAIL_STD_DECLVAL_HPP
+
+#include <boost/hana/detail/std/add_rvalue_reference.hpp>
+
+
+namespace boost { namespace hana { namespace detail { namespace std {
+    template <typename T>
+    typename add_rvalue_reference<T>::type declval();
+}}}}
+
+#endif // !BOOST_HANA_DETAIL_STD_DECLVAL_HPP
