98a39c12048d5d8ec80b6b0463ff7fe348f9856c
Louis Dionne <ldionne.2@gmail.com>
2014-11-07 10:57:48 -0500

    Remove superfluous #define BOOST_PP_VARIADICS 1.

--- test/record_macros.cpp
+++ test/record_macros.cpp
@@ -4,7 +4,6 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-#define BOOST_PP_VARIADICS 1
 #include <boost/hana/foldable.hpp>
 #include <boost/hana/record_macros.hpp>
 #include <boost/hana/searchable.hpp>
