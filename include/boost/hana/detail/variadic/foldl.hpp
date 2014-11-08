39006d288d300d288420f3c1ac1bf0d3302a9711
Louis Dionne <ldionne.2@gmail.com>
2014-11-08 13:18:25 -0500

    Experiment with fold expressions.

--- include/boost/hana/detail/variadic/foldl.hpp
+++ include/boost/hana/detail/variadic/foldl.hpp
@@ -45,7 +45,7 @@ namespace boost { namespace hana { namespace detail { namespace variadic {
 
     struct _foldl {
         template <typename F, typename S, typename ...Xs>
-        constexpr decltype(auto) operator()(F&& f, S&& s, Xs&& ...xs) const { 
+        constexpr decltype(auto) operator()(F&& f, S&& s, Xs&& ...xs) const {
             return foldl1(
                 detail::std::forward<F>(f),
                 detail::std::forward<S>(s),
@@ -55,6 +55,61 @@ namespace boost { namespace hana { namespace detail { namespace variadic {
     };
 
     constexpr _foldl foldl{};
+
+    // Experiment with fold expressions; see N4191 at
+    // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n4191.html
+#if 0
+    template <typename F, typename State>
+    struct accumulator {
+        F f;
+        State state;
+
+        template <typename X>
+        constexpr decltype(auto) operator+(X&& x) const& {
+            return detail::create<variadic::accumulator>{}(
+                f, f(state, detail::std::forward<X>(x))
+            );
+        }
+
+        template <typename X>
+        constexpr decltype(auto) operator+(X&& x) & {
+            return detail::create<variadic::accumulator>{}(
+                f, f(state, detail::std::forward<X>(x))
+            );
+        }
+
+        template <typename X>
+        constexpr decltype(auto) operator+(X&& x) && {
+            decltype(auto) result = f(detail::std::move(state),
+                                      detail::std::forward<X>(x));
+            return detail::create<variadic::accumulator>{}(
+                detail::std::move(f),
+                detail::std::forward<decltype(result)>(result)
+            );
+        }
+
+        constexpr State const& get() const& { return state; }
+        constexpr State & get() & { return state; }
+        constexpr State get() && { return detail::std::move(state); }
+    };
+
+    struct _foldl {
+        template <typename ...Xs>
+        static constexpr decltype(auto) helper(Xs&& ...xs)
+        { return (... + detail::std::forward<Xs>(xs)); }
+
+        template <typename F, typename State, typename ...Xs>
+        constexpr decltype(auto) operator()(F&& f, State&& state, Xs&& ...xs) const {
+            return helper(
+                detail::create<accumulator>{}(
+                    detail::std::forward<F>(f),
+                    detail::std::forward<State>(state)
+                ),
+                detail::std::forward<Xs>(xs)...
+            ).get();
+        }
+    };
+#endif
 }}}} // end namespace boost::hana::detail::variadic
 
 #endif // !BOOST_HANA_DETAIL_VARIADIC_FOLDL_HPP
