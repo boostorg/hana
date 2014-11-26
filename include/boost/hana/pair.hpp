7fbd438f31145d9f235749a1c0d57c6fdddc47ca
Louis Dionne <ldionne.2@gmail.com>
2014-11-26 09:32:11 -0500

    Use perfect forwarding in Pair.

--- include/boost/hana/pair.hpp
+++ include/boost/hana/pair.hpp
@@ -23,10 +23,6 @@ namespace boost { namespace hana {
     //!
     //! ### Example
     //! @snippet example/pair.cpp product
-    //!
-    //! @todo
-    //! Use perfect forwarding in `first_impl` and `second_impl` once
-    //! bug http://llvm.org/bugs/show_bug.cgi?id=20619 is fixed.
     template <>
     struct Product::instance<Pair> : Product::mcd {
         template <typename F, typename S>
@@ -34,12 +30,12 @@ namespace boost { namespace hana {
         { return pair(detail::std::forward<F>(f), detail::std::forward<S>(s)); }
 
         template <typename P>
-        static constexpr auto first_impl(P p)
-        { return p.first; }
+        static constexpr decltype(auto) first_impl(P&& p)
+        { return detail::std::forward<P>(p).first; }
 
         template <typename P>
-        static constexpr auto second_impl(P p)
-        { return p.second; }
+        static constexpr decltype(auto) second_impl(P&& p)
+        { return detail::std::forward<P>(p).second; }
     };
 }} // end namespace boost::hana
 
