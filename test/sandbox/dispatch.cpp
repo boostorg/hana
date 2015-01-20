bc44ea9e49f895cd10eebba2afc63750dc325e6e
Louis Dionne <ldionne.2@gmail.com>
2015-01-19 19:22:27 -0500

    Nested stuff<>

--- test/sandbox/dispatch.cpp
+++ test/sandbox/dispatch.cpp
@@ -5,25 +5,16 @@
 //    use stuff that's not in the library at all.
 // 3.
 
-/////////////////////////////////////////
-// Comparable
-/////////////////////////////////////////
-template <typename T, typename U>
-struct equal_impl {
-    template <typename X, typename Y>
-    static constexpr auto apply(X x, Y y) {
-        return not_(not_equal(x, y));
-    }
-};
+template <typename ...>
+struct wrong { static constexpr bool value = false; };
 
-template <typename T, typename U>
-struct not_equal_impl {
-    template <typename X, typename Y>
-    static constexpr auto apply(X x, Y y) {
-        return not_(equal(x, y));
-    }
-};
+template <typename, typename T>
+struct snd { using type = T; };
+template <typename _, typename T>
+using snd_t = typename snd<_, T>::type;
 
+
+#if 0
 /////////////////////////////////////////
 // Orderable
 //
@@ -252,6 +243,128 @@ struct Iterable {
 
     };
 };
+#endif
+
+#include <iostream>
+
+template <typename Concept, typename T>
+struct stuff : Concept::template stuff<T> { };
+
+/////////////////////////////////////////
+// Comparable
+/////////////////////////////////////////
+struct Comparable {
+    template <typename T, typename U>
+    struct equal_impl {
+        template <typename X, typename Y>
+        static void apply(X x, Y y) {
+            std::cerr << "default equal_impl\n";
+        }
+    };
+
+    template <typename T, typename U>
+    struct not_equal_impl {
+        template <typename X, typename Y>
+        static void apply(X x, Y y) {
+            std::cerr << "default not_equal_impl\n";
+        }
+    };
+
+    template <typename T>
+    struct stuff : snd_t<T, Comparable> { };
+};
+
+template <typename T, typename U>
+struct equal_impl : stuff<Comparable, T>::template equal_impl<T, U> {
+    // static_assert(T is the same as U);
+};
+
+template <typename T, typename U>
+struct not_equal_impl : stuff<Comparable, T>::template not_equal_impl<T, U> {
+    // static_assert(T is the same as U);
+};
+
+
+/////////////////////////////////////////
+// Enumerable
+/////////////////////////////////////////
+struct Enumerable {
+    template <typename E>
+    struct succ_impl {
+        static_assert(wrong<succ_impl<E>>::value,
+        "no definition of boost::hana::succ for the given data type");
+    };
+
+    template <typename E>
+    struct pred_impl {
+        static_assert(wrong<pred_impl<E>>::value,
+        "no definition of boost::hana::pred for the given data type");
+    };
+
+    template <typename E>
+    struct stuff : snd_t<E, Enumerable> { };
+};
+
+template <typename E>
+struct succ_impl : stuff<Enumerable, E>::template succ_impl<E> {
+
+};
+
+template <typename E>
+struct pred_impl : stuff<Enumerable, E>::template pred_impl<E> {
+
+};
+
+/////////////////////////////////////////
+// Constant
+/////////////////////////////////////////
+struct Constant {
+    template <typename C>
+    struct value_impl {
+        static_assert(wrong<value_impl<C>>::value,
+        "no definition of boost::hana::value for the given data type");
+    };
+
+    template <typename C>
+    struct stuff : snd_t<C, Constant> { };
+};
+
+template <typename C>
+struct value_impl : stuff<Constant, C>::template value_impl<C> {
+
+};
+
+#if 1
+/////////////////////////////////////////
+// Monoid
+/////////////////////////////////////////
+struct Monoid {
+    template <typename T, typename U>
+    struct plus_impl {
+        static_assert(wrong<plus_impl<T, U>>::value,
+        "no definition of boost::hana::plus for the given data type");
+    };
+
+    template <typename M>
+    struct zero_impl {
+        static_assert(wrong<zero_impl<M>>::value,
+        "no definition of boost::hana::zero for the given data type");
+    };
+
+    template <typename M>
+    struct stuff : snd_t<M, Monoid> { };
+};
+
+template <typename T, typename U>
+struct plus_impl : stuff<Monoid, T>::template plus_impl<T, U> {
+    // static_assert(T is the same as U);
+};
+
+template <typename M>
+struct zero_impl : stuff<Monoid, M>::template zero_impl<M> {
+
+};
+#endif
 
 /////////////////////////////////////////
 // IntegralConstant
@@ -262,53 +375,105 @@ struct Iterable {
 // - Comparable, Orderable
 // - Monoid, Group, Ring, IntegralDomain
 /////////////////////////////////////////
-template <typename C>
-struct integral_constant_impl {
-    static_assert(wrong<integral_constant_impl<C>>::value,
-    "no definition of boost::hana::integral_constant for the given data type");
-};
+struct IntegralConstant
+    : Constant
+    , Enumerable
+    , Comparable
+    , Monoid
+{
+    template <typename C>
+    struct stuff : snd_t<C, IntegralConstant> { };
 
-struct IntegralConstant {
+    template <typename C>
+    struct integral_constant_impl {
+        static_assert(wrong<integral_constant_impl<C>>::value,
+        "no definition of boost::hana::integral_constant for the given data type");
+    };
+
     // Enumerable
     template <typename C>
     struct succ_impl {
-
+        template <typename X>
+        static void apply(X x) {
+            std::cerr << "succ_impl via IntegralConstant\n";
+        }
     };
 
     template <typename C>
     struct pred_impl {
-
+        template <typename X>
+        static void apply(X x) {
+            std::cerr << "pred_impl via IntegralConstant\n";
+        }
     };
+
+    // Comparable
+    template <typename T, typename U>
+    struct equal_impl {
+        template <typename X, typename Y>
+        static void apply(X x, Y y) {
+            std::cerr << "equal_impl via IntegralConstant\n";
+        }
+    };
+
+    // Monoid
+    template <typename T, typename U>
+    struct plus_impl {
+        template <typename X, typename Y>
+        static void apply(X x, Y y) {
+            std::cerr << "plus_impl via IntegralConstant\n";
+        }
+    };
+
+    template <typename C>
+    struct zero_impl {
+        static void apply() {
+            std::cerr << "zero_impl via IntegralConstant\n";
+        }
+    };
+};
+
+template <typename C>
+struct integral_constant_impl : stuff<IntegralConstant, C>::template integral_constant_impl<C> {
+
 };
 
 
 /////////////////////////////////////////
 // MPLIntegralC
 /////////////////////////////////////////
-struct MPLIntegralC {
-    struct hana {
-        struct Enumerable : IntegralConstant { };
-        struct Monoid {
-            template <typename C>
-            struct zero_impl { /* ... */ };
-
-            template <typename T, typename U>
-            struct plus_impl : IntegralConstant::Monoid::plus_impl<> { /* ... */ };
-        };
-    };
-};
+struct MPLIntegralC;
 
 // Constant
 template <>
 struct value_impl<MPLIntegralC> {
-    // ...
+    template <typename X>
+    static void apply(X x) {
+        std::cerr << "value_impl<MPLIntegralC>\n";
+    }
 };
 
 // IntegralConstant
 template <>
 struct integral_constant_impl<MPLIntegralC> {
-    // ...
+    template <typename X>
+    static void apply(X x) {
+        std::cerr << "integral_constant_impl<MPLIntegralC>\n";
+    }
 };
+
+// Comparable
+#if 0
+template <>
+struct equal_impl<MPLIntegralC, MPLIntegralC>
+    : IntegralConstant::equal_impl<MPLIntegralC, MPLIntegralC>
+{ };
+#else
+template <>
+struct Comparable::stuff<MPLIntegralC>
+    : IntegralConstant
+{ };
+#endif
 
 // Enumerable
 template <>
@@ -324,7 +489,7 @@ struct Monoid::stuff<MPLIntegralC>
 
 // etc...
 
-
+#if 0
 /////////////////////////////////////////
 // Employee
 /////////////////////////////////////////
@@ -338,16 +503,28 @@ struct Employee {
 
 
 // We must be able to call `equal(Employee, Employee)`.
-
+#endif
 
 
 
 
 // Priorities:
 // 1. Explicit/partial specialization
-// ?. Implementation provided by a subclass
+// 2. Implementation provided by subclass
 // 3. Implementation provided by operators
 // 4. Default implementation
 
 
+int main() {
+    succ_impl<MPLIntegralC>::apply(0);
+    pred_impl<MPLIntegralC>::apply(0);
 
+    equal_impl<MPLIntegralC, MPLIntegralC>::apply(0, 0);
+    not_equal_impl<MPLIntegralC, MPLIntegralC>::apply(0, 0);
+
+    value_impl<MPLIntegralC>::apply(0);
+    integral_constant_impl<MPLIntegralC>::apply(0);
+
+    zero_impl<MPLIntegralC>::apply();
+    plus_impl<MPLIntegralC, MPLIntegralC>::apply(0, 0);
+}
