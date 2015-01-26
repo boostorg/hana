6d0ece8cd0f5b17dc7db5bdbfebe4ed916dc474e
Louis Dionne <ldionne.2@gmail.com>
2015-01-26 17:30:17 -0500

    [Monoid] Refactor

--- include/boost/hana/fwd/monoid.hpp
+++ include/boost/hana/fwd/monoid.hpp
@@ -11,25 +11,37 @@ Distributed under the Boost Software License, Version 1.0.
 #define BOOST_HANA_FWD_MONOID_HPP
 
 #include <boost/hana/core/datatype.hpp>
-#include <boost/hana/core/method.hpp>
 #include <boost/hana/detail/std/forward.hpp>
 
 
 namespace boost { namespace hana {
     //! @ingroup group-typeclasses
-    //! The `Monoid` type class is used for data types with an associative
+    //! The `Monoid` concept represents data types with an associative
     //! binary operation that has an identity.
     //!
-    //! The method names refer to the monoid of numbers under addition, but
-    //! there are many other instances such as sequences under concatenation.
-    //! Some datatypes can be viewed as a monoid in more than one way, e.g.
-    //! both addition and multiplication on numbers.
+    //! Specifically, a [Monoid][1] is a basic algebraic structure typically
+    //! used in mathematics to construct more complex algebraic structures
+    //! like `Group`s, `Ring`s and so on. They are useful in several contexts,
+    //! notably to define the properties of numbers in a granular way. At its
+    //! core, a `Monoid` is a set `S` of objects along with a binary operation
+    //! (let's say `+`) that is associative and that has an identity in `S`.
+    //! There are many examples of `Monoid`s:
+    //! - strings with concatenation and the empty string as the identity
+    //! - integers with addition and `0` as the identity
+    //! - integers with multiplication and `1` as the identity
+    //! - many others...
+    //!
+    //! As you can see with the integers, there are some sets that can be
+    //! viewed as a monoid in more than one way, depending on the choice
+    //! of the binary operation and identity. The method names used here
+    //! refer to the monoid of integers under addition; `plus` is the binary
+    //! operation and `zero` is the identity element of that operation.
     //!
     //!
     //! Laws
     //! ----
-    //! For all objects `x`, `y` and `z` whose data type `M` is a `Monoid`,
-    //! the following laws must be satisfied:
+    //! For all objects `x`, `y` and `z` of a `Monoid` `M`, the following
+    //! laws must be satisfied:
     //! @code
     //!     plus(zero<M>(), x) == x                    // left zero
     //!     plus(x, zero<M>()) == x                    // right zero
@@ -37,39 +49,92 @@ namespace boost { namespace hana {
     //! @endcode
     //!
     //!
-    //! Minimal complete definitions
-    //! ----------------------------
-    //! 1. `plus` and `zero`
-    //! @todo
+    //! Minimal complete definition
+    //! ---------------------------
+    //! `plus` and `zero` satisfying the above laws
     //!
-    //! 2. `operator+` and `static_cast`ability to 0
-    //! Any two objects whose data types that can be added with the usual
-    //! `operator+` have a definition of `plus` using this addition. Any
-    //! data type `T` with a valid conversion (using `static_cast`) from
-    //! `int` has a definition of `zero` equivalent to `static_cast<T>(0)`.
     //!
-    //! @todo
-    //! Use `to` instead of `static_cast` to provide the `zero` method.
+    //! Provided models
+    //! ---------------
+    //! 1. For arithmetic data types\n
+    //! A data type `T` is arithmetic if `std::is_arithmetic<T>::%value` is
+    //! true. For an arithmetic data type `T`, a model of `Monoid` is
+    //! automatically defined by setting
+    //! @code
+    //!     plus(x, y) = (x + y)
+    //!     zero<T>() = static_cast<T>(0)
+    //! @endcode
+    //!
+    //!
+    //! Operators
+    //! ---------
+    //! For convenience, the following operator is provided as an
+    //! equivalent way of calling the corresponding method:
+    //! @code
+    //!     + -> plus
+    //! @endcode
+    //!
+    //!
+    //! Structure-preserving functions
+    //! ------------------------------
+    //! Let `A` and `B` be two `Monoid`s. A function `f : A -> B` is said
+    //! to be a [Monoid morphism][2] if it preserves the monoidal structure
+    //! between `A` and `B`. Rigorously, for all objects `x, y` of data
+    //! type `A`,
+    //! @code
+    //!     f(plus(x, y)) == plus(f(x), f(y))
+    //!     f(zero<A>()) == zero<B>()
+    //! @endcode
+    //! Functions with these properties interact nicely with `Monoid`s, which
+    //! is why they are given such a special treatment.
+    //!
+    //!
+    //! [1]: http://en.wikipedia.org/wiki/Monoid
+    //! [2]: http://en.wikipedia.org/wiki/Monoid#Monoid_homomorphisms
     struct Monoid { };
 
-    //! Associative operation on a `Monoid`.
+    //! Associative binary operation on a `Monoid`.
     //! @relates Monoid
     //!
-    //! ### Example
+    //! @param x, y
+    //! Two objects to combine with the `Monoid`'s binary operation.
+    //!
+    //!
+    //! Cross-type version of the method
+    //! --------------------------------
+    //! The `plus` method is "overloaded" to handle distinct data types
+    //! with certain properties. Specifically, `plus` is defined for
+    //! _distinct_ data types `A` and `B` such that
+    //! 1. `A` and `B` share a common data type `C`, as determined by the
+    //!    `common` metafunction
+    //! 2. `A`, `B` and `C` are all `Monoid`s when taken individually
+    //! 3. `to<C> : A -> B` and `to<C> : B -> C` are `Monoid`-embeddings, as
+    //!    determined by the `is_embedding` metafunction.
+    //!
+    //! The definition of `plus` for data types satisfying the above
+    //! properties is obtained by setting
+    //! @code
+    //!     plus(x, y) = plus(to<C>(x), to<C>(y))
+    //! @endcode
+    //!
+    //!
+    //! Example
+    //! -------
     //! @snippet example/monoid.cpp plus
 #ifdef BOOST_HANA_DOXYGEN_INVOKED
     constexpr auto plus = [](auto&& x, auto&& y) -> decltype(auto) {
         return tag-dispatched;
     };
 #else
-    BOOST_HANA_BINARY_METHOD(plus_impl);
+    template <typename T, typename U, typename = void>
+    struct plus_impl;
 
     struct _plus {
         template <typename X, typename Y>
         constexpr decltype(auto) operator()(X&& x, Y&& y) const {
-            return dispatch<plus_impl<
+            return plus_impl<
                 typename datatype<X>::type, typename datatype<Y>::type
-            >>::apply(
+            >::apply(
                 detail::std::forward<X>(x),
                 detail::std::forward<Y>(y)
             );
@@ -85,7 +150,9 @@ namespace boost { namespace hana {
     //! @tparam M
     //! The data type (a `Monoid`) of the returned identity.
     //!
-    //! ### Example
+    //!
+    //! Example
+    //! -------
     //! @snippet example/monoid.cpp zero
 #ifdef BOOST_HANA_DOXYGEN_INVOKED
     template <typename M>
@@ -93,12 +160,13 @@ namespace boost { namespace hana {
         return tag-dispatched;
     };
 #else
-    BOOST_HANA_METHOD(zero_impl);
+    template <typename M, typename = void>
+    struct zero_impl;
 
     template <typename M>
     struct _zero {
         constexpr decltype(auto) operator()() const
-        { return dispatch<zero_impl<M>>::apply(); }
+        { return zero_impl<M>::apply(); }
     };
 
     template <typename M>
--- include/boost/hana/monoid.hpp
+++ include/boost/hana/monoid.hpp
@@ -15,18 +15,21 @@ Distributed under the Boost Software License, Version 1.0.
 #include <boost/hana/core/common.hpp>
 #include <boost/hana/core/convert.hpp>
 #include <boost/hana/core/datatype.hpp>
-#include <boost/hana/core/method.hpp>
 #include <boost/hana/core/models.hpp>
 #include <boost/hana/core/operators.hpp>
 #include <boost/hana/core/when.hpp>
-#include <boost/hana/detail/dispatch_common.hpp>
-#include <boost/hana/detail/std/declval.hpp>
+#include <boost/hana/core/wrong.hpp>
+#include <boost/hana/detail/has_common_embedding.hpp>
 #include <boost/hana/detail/std/enable_if.hpp>
 #include <boost/hana/detail/std/forward.hpp>
 #include <boost/hana/detail/std/integral_constant.hpp>
+#include <boost/hana/detail/std/is_arithmetic.hpp>
 
 
 namespace boost { namespace hana {
+    //////////////////////////////////////////////////////////////////////////
+    // Operators
+    //////////////////////////////////////////////////////////////////////////
     namespace operators {
         //! Equivalent to `plus`.
         //! @relates boost::hana::Monoid
@@ -35,41 +38,65 @@ namespace boost { namespace hana {
             enable_operators<Monoid, datatype_t<Y>>::value
         >::type>
         constexpr decltype(auto) operator+(X&& x, Y&& y) {
-            return plus(
-                detail::std::forward<decltype(x)>(x),
-                detail::std::forward<decltype(y)>(y)
-            );
+            return hana::plus(detail::std::forward<X>(x),
+                              detail::std::forward<Y>(y));
         }
     }
 
-    template <typename T, typename U, typename Context>
-    struct dispatch_impl<4, plus_impl<T, U>, Context>
-        : detail::dispatch_common<plus_impl<T, U>, Monoid, Context>
-    { };
+    //////////////////////////////////////////////////////////////////////////
+    // plus
+    //////////////////////////////////////////////////////////////////////////
+    template <typename T, typename U, typename>
+    struct plus_impl : plus_impl<T, U, when<true>> { };
 
-    template <typename T>
-    struct plus_impl<T, T, when_valid<
-        decltype(detail::std::declval<T>() + detail::std::declval<T>())
-    >> {
+    template <typename T, typename U, bool condition>
+    struct plus_impl<T, U, when<condition>> {
+        static_assert(wrong<plus_impl<T, U>>{},
+        "no definition of boost::hana::plus for the given data types");
+    };
+
+    // Cross-type overload
+    template <typename T, typename U>
+    struct plus_impl<T, U, when<detail::has_common_embedding<Monoid, T, U>{}>> {
+        using C = typename common<T, U>::type;
         template <typename X, typename Y>
         static constexpr decltype(auto) apply(X&& x, Y&& y) {
-            return detail::std::forward<X>(x) + detail::std::forward<Y>(y);
+            return hana::plus(to<C>(detail::std::forward<X>(x)),
+                              to<C>(detail::std::forward<Y>(y)));
         }
     };
 
-    template <typename T>
-    struct zero_impl<T, when_valid<decltype(static_cast<T>(0))>> {
-        static constexpr T apply()
-        { return static_cast<T>(0); }
+    //////////////////////////////////////////////////////////////////////////
+    // zero
+    //////////////////////////////////////////////////////////////////////////
+    template <typename M, typename>
+    struct zero_impl : zero_impl<M, when<true>> { };
+
+    template <typename M, bool condition>
+    struct zero_impl<M, when<condition>> {
+        static_assert(wrong<zero_impl<M>>{},
+        "no definition of boost::hana::zero for the given data type");
     };
 
-    template <>
-    struct models_impl<Monoid> {
-        template <typename T, typename Context>
-        static constexpr auto apply =
-            is_implemented<plus_impl<T, T>, Context> &&
-            is_implemented<zero_impl<T>, Context>
-        ;
+    //////////////////////////////////////////////////////////////////////////
+    // Model for arithmetic data types
+    //////////////////////////////////////////////////////////////////////////
+    template <typename T>
+    struct models<Monoid(T), when<detail::std::is_arithmetic<T>{}>>
+        : detail::std::true_type
+    { };
+
+    template <typename T>
+    struct plus_impl<T, T, when<detail::std::is_arithmetic<T>{}>> {
+        template <typename X, typename Y>
+        static constexpr decltype(auto) apply(X&& x, Y&& y)
+        { return detail::std::forward<X>(x) + detail::std::forward<Y>(y); }
+    };
+
+    template <typename T>
+    struct zero_impl<T, when<detail::std::is_arithmetic<T>{}>> {
+        static constexpr T apply()
+        { return static_cast<T>(0); }
     };
 }} // end namespace boost::hana
 
