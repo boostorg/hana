1683999b4e7eac8407ff7b99b4c9c1fd94bb1a65
Louis Dionne <ldionne.2@gmail.com>
2015-02-08 12:03:21 -0500

    [Detail] Add std::conditional

---
+++ include/boost/hana/detail/std/conditional.hpp
@@ -0,0 +1,24 @@
+/*!
+@file
+Defines an equivalent to `std::conditional`.
+
+@copyright Louis Dionne 2014
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef BOOST_HANA_DETAIL_STD_CONDITIONAL_HPP
+#define BOOST_HANA_DETAIL_STD_CONDITIONAL_HPP
+
+namespace boost { namespace hana { namespace detail { namespace std {
+    template <bool, typename Then, typename Else>
+    struct conditional { using type = Then; };
+
+    template <typename Then, typename Else>
+    struct conditional<false, Then, Else> { using type = Else; };
+
+    template <bool Cond, typename Then, typename Else>
+    using conditional_t = typename conditional<Cond, Then, Else>::type;
+}}}} // end namespace boost::hana::detail::std
+
+#endif // !BOOST_HANA_DETAIL_STD_CONDITIONAL_HPP
