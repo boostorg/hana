7021c2dd3c0acd2524a26eb9498ab3a6fb1d9d03
Louis Dionne <ldionne.2@gmail.com>
2015-04-02 00:56:42 -0400

    [Travis] Move to the container-based infrastructure
    - Build libc++ properly on Travis (with CMake)
    - Test against Boost trunk
    - Build the documentation and push to gh-pages when on master

--- oom_respawn.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for job in 1 2 3 4 5 6; do
-    if (travis show ${TRAVIS_BUILD_NUMBER}.${job} --repo ldionne/hana | grep --quiet -e "failed" -e "errored"); then
-        if (travis logs ${TRAVIS_BUILD_NUMBER}.${job} --repo ldionne/hana --no-stream | grep --quiet -e "OOM" -e "Your test run exceeded 50 minutes"); then
-            travis restart ${TRAVIS_BUILD_NUMBER}.${job} --repo ldionne/hana
-        fi
-    fi
-done
