0612624939502656f97b78ca7e23f2d1424a81cd
Louis Dionne <ldionne.2@gmail.com>
2015-04-14 13:32:45 -0400

    [Detail] Add basic unit tests for the constexpr algorithms.

--- include/boost/hana/detail/constexpr/algorithm.hpp
+++ include/boost/hana/detail/constexpr/algorithm.hpp
@@ -128,7 +128,7 @@ namespace boost { namespace hana { namespace detail { namespace constexpr_ {
     template <typename InputIter, typename UnaryPred>
     constexpr InputIter find_if(InputIter first, InputIter last, UnaryPred pred) {
         for (; first != last; ++first)
-            if (p(*first))
+            if (pred(*first))
                 return first;
         return last;
     }
