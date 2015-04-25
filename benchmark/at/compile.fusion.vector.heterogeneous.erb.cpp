boostorg/hana

commit ca37fdfa4c2950c3434bcd74341d5b4e8942ae09

[Doc] Add a section on performance in the tutorial
Also:
- Introduce the .js charts we have in the reference
- Remove the ugly "Generated by Doxygen" footer
- Refactor the benchmarks

benchmark/at/compile.fusion.vector.heterogeneous.erb.cpp
@@ -4,12 +4,9 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-<% if input_size > 10 %>
-    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
-<% end %>
-
 #include <boost/fusion/include/at.hpp>
 #include <boost/fusion/include/make_vector.hpp>
+#include <boost/fusion/include/push_back.hpp>
 namespace fusion = boost::fusion;
 
 
@@ -17,9 +14,7 @@ template <int>
 struct x { };
 
 int main() {
-    auto vector = fusion::make_vector(
-        <%= (1..input_size).map { |n| "x<#{n}>{}" }.join(', ') %>
-    );
+    auto vector = <%= fusion_vector((1..input_size).map { |n| "x<#{n}>{}" }) %>;
     auto result = fusion::at_c<<%= input_size-1 %>>(vector);
     (void)result;
 }

benchmark/at/compile.fusion.vector.homogeneous.erb.cpp
@@ -4,19 +4,14 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-<% if input_size > 10 %>
-    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
-<% end %>
-
 #include <boost/fusion/include/at.hpp>
 #include <boost/fusion/include/make_vector.hpp>
+#include <boost/fusion/include/push_back.hpp>
 namespace fusion = boost::fusion;
 
 
 int main() {
-    auto vector = fusion::make_vector(
-        <%= (1..input_size).map { |n| "#{n}" }.join(', ') %>
-    );
+    auto vector = <%= fusion_vector((1..input_size).map { |n| "#{n}" }) %>;
     auto result = fusion::at_c<<%= input_size-1 %>>(vector);
     (void)result;
 }

benchmark/fold_right/compile.fusion.vector.homogeneous.erb.cpp
@@ -4,12 +4,9 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-<% if input_size > 10 %>
-    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
-<% end %>
-
-#include <boost/fusion/include/reverse_fold.hpp>
 #include <boost/fusion/include/make_vector.hpp>
+#include <boost/fusion/include/push_back.hpp>
+#include <boost/fusion/include/reverse_fold.hpp>
 
 
 struct f {
@@ -22,9 +19,7 @@ struct state { };
 struct x { };
 
 int main() {
-    auto vector = boost::fusion::make_vector(
-        <%= (1..input_size).map { "x{}" }.join(', ') %>
-    );
+    auto vector = <%= fusion_vector((1..input_size).map { "x{}" }) %>;
     auto result = boost::fusion::reverse_fold(vector, state{}, f{});
     (void)result;
 }

benchmark/make/compile.hana.tuple_c.erb.cpp
@@ -1,0 +1,15 @@
+/*
+@copyright Louis Dionne 2015
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+ */
+
+#include <boost/hana/tuple.hpp>
+
+
+int main() {
+    constexpr auto tuple = boost::hana::tuple_c<int,
+        <%= (1..input_size).map { |n| "#{n}" }.join(', ') %>
+    >;
+    (void)tuple;
+}

benchmark/measure.in.rb
@@ -29,7 +29,9 @@ def split_at(n, list)
 end
 
 # types : A sequence of strings to put in the mpl::vector.
-# Using this method requires including <boost/mpl/push_back.hpp>
+# Using this method requires including
+#   - <boost/mpl/vector.hpp>
+#   - <boost/mpl/push_back.hpp>
 def mpl_vector(types)
   fast, rest = split_at(20, types)
   rest.inject("boost::mpl::vector#{fast.length}<#{fast.join(', ')}>") { |v, t|
@@ -38,11 +40,24 @@ def mpl_vector(types)
 end
 
 # types : A sequence of strings to put in the mpl::list.
-# Using this method requires including <boost/mpl/push_front.hpp>
+# Using this method requires including
+#   - <boost/mpl/list.hpp>
+#   - <boost/mpl/push_front.hpp>
 def mpl_list(types)
   prefix, fast = split_at([types.length - 20, 0].max, types)
   prefix.reverse.inject("boost::mpl::list#{fast.length}<#{fast.join(', ')}>") { |l, t|
     "boost::mpl::push_front<#{l}, #{t}>::type"
+  }
+end
+
+# values : A sequence of strings representing values to put in the fusion::vector.
+# Using this method requires including
+#   - <boost/fusion/include/make_vector.hpp>
+#   - <boost/fusion/include/push_back.hpp>
+def fusion_vector(values)
+  fast, rest = split_at(10, values)
+  rest.inject("boost::fusion::make_vector(#{fast.join(', ')})") { |xs, v|
+    "boost::fusion::push_back(#{xs}, #{v})"
   }
 end
 

benchmark/transform/compile.fusion.vector.heterogeneous.erb.cpp
@@ -4,13 +4,9 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-
-<% if input_size > 10 %>
-    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
-<% end %>
-
 #include <boost/fusion/include/as_vector.hpp>
 #include <boost/fusion/include/make_vector.hpp>
+#include <boost/fusion/include/push_back.hpp>
 #include <boost/fusion/include/transform.hpp>
 
 
@@ -23,9 +19,7 @@ template <int i>
 struct x { };
 
 int main() {
-    auto vector = boost::fusion::make_vector(
-        <%= (1..input_size).map { |n| "x<#{n}>{}" }.join(', ') %>
-    );
+    auto vector = <%= fusion_vector((1..input_size).map { |n| "x<#{n}>{}" }) %>;
     auto result = boost::fusion::as_vector(boost::fusion::transform(vector, f{}));
     (void)result;
 }

benchmark/transform/compile.fusion.vector.homogeneous.erb.cpp
@@ -4,12 +4,9 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-<% if input_size > 10 %>
-    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
-<% end %>
-
 #include <boost/fusion/include/as_vector.hpp>
 #include <boost/fusion/include/make_vector.hpp>
+#include <boost/fusion/include/push_back.hpp>
 #include <boost/fusion/include/transform.hpp>
 
 
@@ -21,9 +18,7 @@ struct f {
 struct x { };
 
 int main() {
-    auto vector = boost::fusion::make_vector(
-        <%= (1..input_size).map { |n| "x{}" }.join(', ') %>
-    );
+    auto vector = <%= fusion_vector((1..input_size).map { "x{}" }) %>;
     auto result = boost::fusion::as_vector(boost::fusion::transform(vector, f{}));
     (void)result;
 }

benchmark/count_if/compile.fusion.vector.homogeneous.erb.cpp
@@ -4,12 +4,9 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-<% if input_size > 10 %>
-    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
-<% end %>
-
 #include <boost/fusion/include/count_if.hpp>
 #include <boost/fusion/include/make_vector.hpp>
+#include <boost/fusion/include/push_back.hpp>
 
 
 struct is_even {
@@ -18,9 +15,7 @@ struct is_even {
 };
 
 int main() {
-    auto vector = boost::fusion::make_vector(
-        <%= (1..input_size).map { |n| "#{n}" }.join(', ') %>
-    );
+    auto vector = <%= fusion_vector((1..input_size).map { |n| "#{n}" }) %>;
     auto result = boost::fusion::count_if(vector, is_even{});
     (void)result;
 }

commit f0ca4df2da9b07d5c534b9bef5de9e694ec3ee3c

[Benchmarks] Fix benchmarks broken by ca37fdf
Also check the benchmarks when we're on develop, to make sure they're
not broken.

.travis.yml
@@ -160,19 +160,18 @@ script:
   - if [[ "${DOCUMENTATION}" == "true" && ("${TRAVIS_PULL_REQUEST}" == "true" || "${TRAVIS_BRANCH}" != "master") ]]; then make doc; fi
   - if [[ "${DOCUMENTATION}" == "true" && "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then make gh-pages.push &>/dev/null; fi
 
-  # We only run the benchmarks on `master` when we're not in a pull request, because
-  # otherwise it takes too much Travis resources. Note that we don't exit earlier in the
-  # script because we still want to make sure our dependencies can be installed when we
-  # push on develop. This is just to minimize the chances of breaking the build on master.
+  # We only run the full benchmarks on `master` when we're not in a pull request, because
+  # otherwise it takes too much Travis resources. Otherwise, we only run partial benchmarks
+  # to make sure they compile/run properly by using the 'check.benchmarks' target.
   #
   # When we run the benchmarks, we pre-clone the datasets branch, so the next datasets.clone
   # will just pull, which is pretty quick. This reduces the risk of push conflicts when these
   # jobs all run at the same time. When we push, we use travis_retry to try again if we encounter
   # a push conflict, and we disable the output to avoid leaking the GitHub token.
-  - if [[ "${BENCHMARKS}" == "true" && ("${TRAVIS_PULL_REQUEST}" == "true" || "${TRAVIS_BRANCH}" != "master") ]]; then exit; fi
-  - if [[ "${BENCHMARKS}" == "true" ]]; then make benchmarks -k; fi
-  - if [[ "${BENCHMARKS}" == "true" ]]; then make datasets.clone; fi
-  - if [[ "${BENCHMARKS}" == "true" ]]; then travis_retry make datasets.push &>/dev/null; fi
+  - if [[ "${BENCHMARKS}" == "true" && ("${TRAVIS_PULL_REQUEST}" == "true" || "${TRAVIS_BRANCH}" != "master") ]]; then make check.benchmarks -k; fi
+  - if [[ "${BENCHMARKS}" == "true" && "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then make benchmarks -k; fi
+  - if [[ "${BENCHMARKS}" == "true" && "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then make datasets.clone; fi
+  - if [[ "${BENCHMARKS}" == "true" && "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then travis_retry make datasets.push &>/dev/null; fi
 
   - if [[ "${UNIT_TESTS}" == "true" ]]; then make compile.headers compile.examples -k -j2 &>/dev/null; fi
   - if [[ "${UNIT_TESTS}" == "true" ]]; then make compile.all -k; fi

benchmark/CMakeLists.txt
@@ -64,6 +64,9 @@ set_target_properties(compile.benchmark.measure
 # Add the benchmarks
 ##############################################################################
 add_custom_target(benchmarks COMMENT "Generate all the benchmarks.")
+add_custom_target(check.benchmarks
+    COMMENT "Partially generate the benchmarks, just to check they compile/run.")
+
 # The list of all the benchmark targets; it is filled just below.
 set(BOOST_HANA_BENCHMARKS "")
 file(GLOB_RECURSE BOOST_HANA_JSON_TEMPLATES *.erb.json)
@@ -85,6 +88,16 @@ foreach(_template IN LISTS BOOST_HANA_JSON_TEMPLATES)
     )
     add_custom_target(${_target} DEPENDS "${output_file}")
     add_dependencies(benchmarks ${_target})
+
+    add_custom_target(check.${_target}
+        COMMAND ${CMAKE_COMMAND} -E env check.benchmarks=true
+        ${RUBY_EXECUTABLE} -r tilt -r ${CMAKE_CURRENT_BINARY_DIR}/measure.rb
+            -e "env = {has_boost: ${Boost_FOUND}, has_mpl11: ${MPL11_FOUND}}    "
+            -e "Tilt::ERBTemplate.new('${_template}').render(nil, **env)        "
+        WORKING_DIRECTORY ${_template_dir}
+        VERBATIM
+    )
+    add_dependencies(check.benchmarks check.${_target})
 endforeach()
 
 
@@ -93,29 +106,40 @@ endforeach()
 #
 # We have a branch named 'datasets'. This branch contains nothing, except
 # directories and JSON files with the following layout:
-#   compiler1/
-#       benchmark1.json
+#   debug/
+#       compiler1/
+#           benchmark1.json
+#           ...
+#           benchmarkN.json
 #       ...
-#       benchmarkN.json
-#   ...
-#   compilerN/
-#       benchmark1.json
+#       compilerN/
+#           benchmark1.json
+#           ...
+#           benchmarkN.json
+#   release/
 #       ...
-#       benchmarkN.json
 #
-# The `compilerN` subdirectories are what we call the `COMPILER_SLUG` below.
+#   default/
+#       ...
+#
+# The `debug`, `release` or `default` are what we call the `BUILD_TYPE_SLUG`
+# below, and it corresponds to the CMake configuration under which the
+# benchmarks were run.
+#
+# The `compilerN` subdirectories are what we call the `COMPILER_SLUG` below,
+# ad it corresponds to the compiler used to compile the benchmarks.
+#
 # The `benchmarkN.json` files are those generated by the targets we set up
 # just above. The 'datasets.clone' target we define below allows cloning
 # the 'dataset' branch to the 'benchmark/dataset' subdirectory of the project,
 # or pulling the latest revisions if the clone already exists. This directory
 # is ignored by git to avoid polluting the index. Then, the 'datasets.clean'
-# target allows removing the subdirectory associated to the current compiler
-# slug (e.g. 'benchmark/datasets/clang-3.6.0'), and the 'datasets.copy' target
-# allows populating that subdirectory with the newly built benchmarks. Finally,
-# the 'datasets.commit' target allows committing the newly built benchmarks to
-# the 'datasets' branch, and the 'datasets.push' target allows pushing those
-# changes to the official repository. Of course, write access is needed to
-# push the changes.
+# target allows removing the subdirectory associated to the current build type
+# and compiler, and the 'datasets.copy' target allows populating that
+# subdirectory with the newly built benchmarks. Finally, the 'datasets.commit'
+# target allows committing the newly built benchmarks to the 'datasets' branch,
+# and the 'datasets.push' target allows pushing those changes to the official
+# repository. Of course, write access is needed to push the changes.
 #
 # Most of this is only used by our continuous integration scripts, but it is
 # also possible to do it manually.

benchmark/at/compile.fusion.vector.heterogeneous.erb.cpp
@@ -4,9 +4,12 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
+<% if input_size > 10 %>
+    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
+<% end %>
+
 #include <boost/fusion/include/at.hpp>
 #include <boost/fusion/include/make_vector.hpp>
-#include <boost/fusion/include/push_back.hpp>
 namespace fusion = boost::fusion;
 
 
@@ -14,7 +17,9 @@ template <int>
 struct x { };
 
 int main() {
-    auto vector = <%= fusion_vector((1..input_size).map { |n| "x<#{n}>{}" }) %>;
+    auto vector = fusion::make_vector(
+        <%= (1..input_size).map { |n| "x<#{n}>{}" }.join(', ') %>
+    );
     auto result = fusion::at_c<<%= input_size-1 %>>(vector);
     (void)result;
 }

benchmark/at/compile.fusion.vector.homogeneous.erb.cpp
@@ -4,14 +4,19 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
+<% if input_size > 10 %>
+    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
+<% end %>
+
 #include <boost/fusion/include/at.hpp>
 #include <boost/fusion/include/make_vector.hpp>
-#include <boost/fusion/include/push_back.hpp>
 namespace fusion = boost::fusion;
 
 
 int main() {
-    auto vector = <%= fusion_vector((1..input_size).map { |n| "#{n}" }) %>;
+    auto vector = fusion::make_vector(
+        <%= (1..input_size).map { |n| "#{n}" }.join(', ') %>
+    );
     auto result = fusion::at_c<<%= input_size-1 %>>(vector);
     (void)result;
 }

benchmark/fold_right/compile.fusion.vector.heterogeneous.erb.cpp
@@ -4,9 +4,13 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
+<% if input_size > 10 %>
+    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
+<% end %>
+
 #include <boost/fusion/include/make_vector.hpp>
-#include <boost/fusion/include/push_back.hpp>
 #include <boost/fusion/include/reverse_fold.hpp>
+namespace fusion = boost::fusion;
 
 
 struct f {
@@ -20,7 +24,9 @@ template <int i>
 struct x { };
 
 int main() {
-    auto vector = <%= fusion_vector((1..input_size).map { |n| "x<#{n}>{}" }) %>;
+    auto vector = fusion::make_vector(
+        <%= (1..input_size).map { |n| "x<#{n}>{}" }.join(', ') %>
+    );
     auto result = boost::fusion::reverse_fold(vector, state{}, f{});
     (void)result;
 }

benchmark/fold_right/compile.fusion.vector.homogeneous.erb.cpp
@@ -4,9 +4,13 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
+<% if input_size > 10 %>
+    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
+<% end %>
+
 #include <boost/fusion/include/make_vector.hpp>
-#include <boost/fusion/include/push_back.hpp>
 #include <boost/fusion/include/reverse_fold.hpp>
+namespace fusion = boost::fusion;
 
 
 struct f {
@@ -19,7 +23,9 @@ struct state { };
 struct x { };
 
 int main() {
-    auto vector = <%= fusion_vector((1..input_size).map { "x{}" }) %>;
+    auto vector = fusion::make_vector(
+        <%= (1..input_size).map { "x{}" }.join(', ') %>
+    );
     auto result = boost::fusion::reverse_fold(vector, state{}, f{});
     (void)result;
 }

benchmark/make/compile.hana.tuple_c.erb.cpp
@@ -8,8 +8,8 @@ Distributed under the Boost Software License, Version 1.0.
 
 
 int main() {
-    constexpr auto tuple = boost::hana::tuple_c<int,
-        <%= (1..input_size).map { |n| "#{n}" }.join(', ') %>
+    constexpr auto tuple = boost::hana::tuple_c<
+        <%= (['int'] + (1..input_size).to_a).join(', ') %>
     >;
     (void)tuple;
 }

benchmark/measure.in.rb
@@ -67,6 +67,10 @@ def measure(aspect, template_relative, range)
   template = Pathname.new(template_relative).expand_path
   range = range.to_a
 
+  if ENV["check.benchmarks"] && range.length >= 2
+    range = [range[0], range[-1]]
+  end
+
   make = -> (target) {
     command = "@CMAKE_COMMAND@ --build @CMAKE_BINARY_DIR@ --target #{target}"
     stdout, stderr, status = Open3.capture3(command)

benchmark/product/compile.hana.tuple_c.erb.cpp
@@ -9,8 +9,8 @@ Distributed under the Boost Software License, Version 1.0.
 
 
 int main() {
-    constexpr auto tuple = boost::hana::tuple_c<unsigned long long,
-        <%= (1..input_size).to_a.join(', ') %>
+    constexpr auto tuple = boost::hana::tuple_c<
+        <%= (['unsigned long long'] + (1..input_size).to_a).join(', ') %>
     >;
     constexpr auto result = boost::hana::product<
         boost::hana::IntegralConstant<unsigned long long>

benchmark/transform/compile.fusion.vector.heterogeneous.erb.cpp
@@ -4,10 +4,14 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
+<% if input_size > 10 %>
+    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
+<% end %>
+
 #include <boost/fusion/include/as_vector.hpp>
 #include <boost/fusion/include/make_vector.hpp>
-#include <boost/fusion/include/push_back.hpp>
 #include <boost/fusion/include/transform.hpp>
+namespace fusion = boost::fusion;
 
 
 struct f {
@@ -19,7 +23,9 @@ template <int i>
 struct x { };
 
 int main() {
-    auto vector = <%= fusion_vector((1..input_size).map { |n| "x<#{n}>{}" }) %>;
-    auto result = boost::fusion::as_vector(boost::fusion::transform(vector, f{}));
+    auto vector = fusion::make_vector(
+        <%= (1..input_size).map { |n| "x<#{n}>{}" }.join(', ') %>
+    );
+    auto result = fusion::as_vector(fusion::transform(vector, f{}));
     (void)result;
 }

benchmark/transform/compile.fusion.vector.homogeneous.erb.cpp
@@ -4,10 +4,14 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
+<% if input_size > 10 %>
+    #define FUSION_MAX_VECTOR_SIZE <%= ((input_size + 9) / 10) * 10 %>
+<% end %>
+
 #include <boost/fusion/include/as_vector.hpp>
 #include <boost/fusion/include/make_vector.hpp>
-#include <boost/fusion/include/push_back.hpp>
 #include <boost/fusion/include/transform.hpp>
+namespace fusion = boost::fusion;
 
 
 struct f {
@@ -18,7 +22,9 @@ struct f {
 struct x { };
 
 int main() {
-    auto vector = <%= fusion_vector((1..input_size).map { "x{}" }) %>;
-    auto result = boost::fusion::as_vector(boost::fusion::transform(vector, f{}));
+    auto vector = fusion::make_vector(
+        <%= (1..input_size).map { "x{}" }.join(', ') %>
+    );
+    auto result = fusion::as_vector(fusion::transform(vector, f{}));
     (void)result;
 }