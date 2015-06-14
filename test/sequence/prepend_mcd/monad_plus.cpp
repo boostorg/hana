2f4bd7168680065468572a3e96e01475e334e95f
Louis Dionne <ldionne.2@gmail.com>
2015-06-14 12:51:37 -0400

    [Tests] Group the unit tests for Foldable and Sequence mcds

--- test/sequence/prepend_mcd/monad_plus.cpp
+++
@@ -1,9 +0,0 @@
-/*
-@copyright Louis Dionne 2015
-Distributed under the Boost Software License, Version 1.0.
-(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
- */
-
-#define BOOST_HANA_TEST_SEQUENCE_PREPEND_MCD
-#define BOOST_HANA_TEST_MONAD_PLUS
-#include <laws/templates/seq.hpp>
