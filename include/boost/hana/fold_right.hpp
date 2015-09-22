403afe2dc9460f3c06e2583ef3f7800004033098
Louis Dionne <ldionne.2@gmail.com>
2015-09-22 08:22:31 -0700

    [folds] Fix bugs in folds when taking state by reference
    Partially addresses this SO question: http://stackoverflow.com/q/32702383/627587

--- include/boost/hana/fold_right.hpp
+++ include/boost/hana/fold_right.hpp
@@ -54,17 +54,29 @@ namespace boost { namespace hana {
     }
     //! @endcond
 
+    namespace detail {
+        template <typename F, typename State>
+        struct variadic_foldr {
+            F& f;
+            State& state;
+            template <typename ...T>
+            constexpr decltype(auto) operator()(T&& ...t) const {
+                return detail::variadic::foldr(
+                    static_cast<F&&>(f),
+                    static_cast<State&&>(state),
+                    static_cast<T&&>(t)...
+                );
+            }
+        };
+    }
+
     template <typename T, bool condition>
     struct fold_right_impl<T, when<condition>> : default_ {
         // with state
         template <typename Xs, typename S, typename F>
         static constexpr decltype(auto) apply(Xs&& xs, S&& s, F&& f) {
             return hana::unpack(static_cast<Xs&&>(xs),
-                hana::partial(
-                    detail::variadic::foldr,
-                    static_cast<F&&>(f),
-                    static_cast<S&&>(s)
-                )
+                detail::variadic_foldr<F, S>{f, s}
             );
         }
 
