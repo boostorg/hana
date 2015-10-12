6ce5826b75faa1dfba2749638253c3f6ce7c7f5c
Louis Dionne <ldionne.2@gmail.com>
2015-10-12 10:39:58 -0400

    [Struct] Return a reference from at_key
    Partially addresses #90

--- include/boost/hana/at_key.hpp
+++ include/boost/hana/at_key.hpp
@@ -12,10 +12,17 @@ Distributed under the Boost Software License, Version 1.0.
 
 #include <boost/hana/fwd/at_key.hpp>
 
+#include <boost/hana/accessors.hpp>
 #include <boost/hana/concept/searchable.hpp>
+#include <boost/hana/concept/struct.hpp>
 #include <boost/hana/core/dispatch.hpp>
+#include <boost/hana/equal.hpp>
 #include <boost/hana/find.hpp>
+#include <boost/hana/find_if.hpp>
+#include <boost/hana/first.hpp>
+#include <boost/hana/functional/on.hpp>
 #include <boost/hana/optional.hpp>
+#include <boost/hana/second.hpp>
 
 
 namespace boost { namespace hana {
@@ -42,6 +49,17 @@ namespace boost { namespace hana {
         static constexpr decltype(auto) apply(Xs&& xs, Key&& key) {
             return hana::from_just(hana::find(static_cast<Xs&&>(xs),
                                               static_cast<Key&&>(key)));
+        }
+    };
+
+    template <typename S>
+    struct at_key_impl<S, when<hana::Struct<S>::value>> {
+        template <typename X, typename Key>
+        static constexpr decltype(auto) apply(X&& x, Key const& key) {
+            auto accessor = hana::second(*hana::find_if(hana::accessors<S>(),
+                hana::equal.to(key) ^hana::on^ hana::first
+            ));
+            return accessor(static_cast<X&&>(x));
         }
     };
 }} // end namespace boost::hana
