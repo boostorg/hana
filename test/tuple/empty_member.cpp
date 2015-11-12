e7ce08e7120a7bc3d2fe63fda40ec3caa0bca20d
Louis Dionne <ldionne.2@gmail.com>
2015-11-11 20:24:08 -0500

    Revert the two previous commits about EBO on Windows
    The commits are db487b2b4 and 76a0ac16a. They were supposed to fix
    unit tests failing on Windows due to a missing EBO, but both of these
    commits failed to do so. Since I can't test on Windows by myself for
    the time being, I'm rolling back these commits and I'll try to fix
    this error later.

--- test/tuple/empty_member.cpp
+++ test/tuple/empty_member.cpp
@@ -7,25 +7,29 @@ Distributed under the Boost Software License, Version 1.0.
 #include <boost/hana/tuple.hpp>
 namespace hana = boost::hana;
 
-//
-// This test makes sure that `hana::tuple` has a simple enough representation.
-// In particular, we make sure that the empty base-class optimization (EBO)
-// is able to fire, when supported.
-//
-
-template <typename T>
-struct holder { T value; };
-
-template <typename ...T>
-struct inherit : T... { };
 
 struct A { };
 struct B { };
 
-static_assert((sizeof(hana::tuple<int, A>) == sizeof(inherit<holder<int>, A>)), "");
-static_assert((sizeof(hana::tuple<A, int>) == sizeof(inherit<A, holder<int>>)), "");
-static_assert((sizeof(hana::tuple<A, int, B>) == sizeof(inherit<A, holder<int>, B>)), "");
-static_assert((sizeof(hana::tuple<A, B, int>) == sizeof(inherit<A, B, holder<int>>)), "");
-static_assert((sizeof(hana::tuple<int, A, B>) == sizeof(inherit<holder<int>, A, B>)), "");
-
-int main() { }
+int main() {
+    {
+        using T = hana::tuple<int, A>;
+        static_assert((sizeof(T) == sizeof(int)), "");
+    }
+    {
+        using T = hana::tuple<A, int>;
+        static_assert((sizeof(T) == sizeof(int)), "");
+    }
+    {
+        using T = hana::tuple<A, int, B>;
+        static_assert((sizeof(T) == sizeof(int)), "");
+    }
+    {
+        using T = hana::tuple<A, B, int>;
+        static_assert((sizeof(T) == sizeof(int)), "");
+    }
+    {
+        using T = hana::tuple<int, A, B>;
+        static_assert((sizeof(T) == sizeof(int)), "");
+    }
+}
