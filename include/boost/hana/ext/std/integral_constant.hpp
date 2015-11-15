a9b23528b7ec41ba7058852a0dfd3dd351f15be7
Louis Dionne <ldionne.2@gmail.com>
2015-11-15 11:40:33 -0500

    [std::integral_constant] Add documentation

--- include/boost/hana/ext/std/integral_constant.hpp
+++ include/boost/hana/ext/std/integral_constant.hpp
@@ -19,6 +19,25 @@ Distributed under the Boost Software License, Version 1.0.
 #include <type_traits>
 
 
+#ifdef BOOST_HANA_DOXYGEN_INVOKED
+namespace std {
+    //! @ingroup group-ext-std
+    //! Adapter for `std::integral_constant`s.
+    //!
+    //! Provided models
+    //! ---------------
+    //! 1. `Constant` and `IntegralConstant`\n
+    //! A `std::integral_constant` is a model of the `IntegralConstant` and
+    //! `Constant` concepts, just like `hana::integral_constant`s are. As a
+    //! consequence, they are also implicitly a model of the concepts provided
+    //! for all models of `Constant`.
+    //! @include example/ext/std/integral_constant.cpp
+    template <typename T, T v>
+    struct integral_constant { };
+}
+#endif
+
+
 namespace boost { namespace hana {
     namespace ext { namespace std {
         template <typename T>
