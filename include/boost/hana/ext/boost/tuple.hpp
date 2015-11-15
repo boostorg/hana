20e0e7d3c4a834f0ecef0fa13ce20ebb90eaa3f2
Louis Dionne <ldionne.2@gmail.com>
2015-11-15 11:49:51 -0500

    [boost::tuple] Add documentation

--- include/boost/hana/ext/boost/tuple.hpp
+++ include/boost/hana/ext/boost/tuple.hpp
@@ -26,6 +26,26 @@ Distributed under the Boost Software License, Version 1.0.
 #include <utility>
 
 
+#ifdef BOOST_HANA_DOXYGEN_INVOKED
+namespace boost {
+    //! @ingroup group-ext-boost
+    //! Adapter for `boost::tuple`s.
+    //!
+    //!
+    //! Modeled concepts
+    //! ----------------
+    //! A `boost::tuple` is a model of the `Sequence` concept, and all the
+    //! concepts it refines. That makes it essentially the same as a Hana
+    //! tuple, although the complexity of some operations might differ from
+    //! that of Hana's tuple.
+    //!
+    //! @include example/ext/boost/tuple.cpp
+    template <typename ...T>
+    struct tuple { };
+}
+#endif
+
+
 namespace boost { namespace hana {
     namespace ext { namespace boost { struct tuple_tag; }}
 
