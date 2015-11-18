bf834ece194a1f7a3fc66ea08ee28eea780aab18
Louis Dionne <ldionne.2@gmail.com>
2015-11-18 11:41:49 -0500

    [Windows] Disable failing tests due to EBO bug, and fix broken example
    The example/tap.cpp bug was actually interesting, so I'll document it
    here. We were using `tap` with a function that performed side effects.
    However, we relied on the side effects being done in a particular order
    for the example to pass. This is a violation of what the documentation
    says we're allowed to.
    
    On Windows, the call convention is such that arguments are evaluated
    from right to left, instead of left to right like on OS X and Linux.
    Since `chain` uses `transform` and `transform` expands `f(x)...` inside
    a function call, our function with side effects was being called in
    reverse order as what we would have expected!
    
    Morale: Listen to the documentation, or perish.

--- example/tap.cpp
+++ example/tap.cpp
@@ -5,23 +5,26 @@ Distributed under the Boost Software License, Version 1.0.
  */
 
 #include <boost/hana/assert.hpp>
+#include <boost/hana/chain.hpp>
 #include <boost/hana/equal.hpp>
 #include <boost/hana/tap.hpp>
 #include <boost/hana/tuple.hpp>
 
-#include <sstream>
+#include <set>
 namespace hana = boost::hana;
 
 
 int main() {
-    std::stringstream before, after;
+    // We use a sorted container because the order in which the functions
+    // are called is unspecified.
+    std::set<int> before, after;
 
     auto xs = hana::make_tuple(1, 2, 3)
-        | hana::tap<hana::tuple_tag>([&](auto x) { before << x << ' '; })
+        | hana::tap<hana::tuple_tag>([&](int x) { before.insert(x); })
         | [](auto x) { return hana::make_tuple(x, -x); }
-        | hana::tap<hana::tuple_tag>([&](auto x) { after << x << ' '; });
+        | hana::tap<hana::tuple_tag>([&](int x) { after.insert(x); });
 
-    BOOST_HANA_RUNTIME_CHECK(before.str() == "1 2 3 ");
-    BOOST_HANA_RUNTIME_CHECK(after.str() == "1 -1 2 -2 3 -3 ");
+    BOOST_HANA_RUNTIME_CHECK(before == std::set<int>{1, 2, 3});
+    BOOST_HANA_RUNTIME_CHECK(after == std::set<int>{1, -1, 2, -2, 3, -3});
     BOOST_HANA_RUNTIME_CHECK(xs == hana::make_tuple(1, -1, 2, -2, 3, -3));
 }
