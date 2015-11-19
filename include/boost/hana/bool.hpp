87f82e9003eaafb29f813a20880e3d8728cbd30d
badair <badair@harding.edu>
2015-11-19 02:01:21 -0600

    [feature] [UDL] adding UDL logic for hex, binary, octal
    [ldionne:
    Remove rogue whitespace at the end of lines
    
    Closes #210]

--- include/boost/hana/bool.hpp
+++ include/boost/hana/bool.hpp
@@ -133,16 +133,59 @@ BOOST_HANA_NAMESPACE_BEGIN
     // User-defined literal
     //////////////////////////////////////////////////////////////////////////
     namespace ic_detail {
-        constexpr int to_int(char c)
-        { return static_cast<int>(c) - 48; }
 
-        template <std::size_t N>
-        constexpr long long parse(const char (&arr)[N]) {
-            long long number = 0, base = 1;
-            for (std::size_t i = 0; i < N; ++i) {
-                number += to_int(arr[N - 1 - i]) * base;
-                base *= 10;
+        constexpr int to_int(char c) {
+            int result = 0;
+
+            if (c >= 'A' && c <= 'F') {
+                result = static_cast<int>(c) - static_cast<int>('A') + 10;
             }
+            else if (c >= 'a' && c <= 'f') {
+                result = static_cast<int>(c) - static_cast<int>('a') + 10;
+            }
+            else {
+                result = static_cast<int>(c) - static_cast<int>('0');
+            }
+
+            return result;
+        }
+
+        template<std::size_t N>
+        constexpr long long parse(const char (&arr)[N]) {
+            long long base = 10;
+            std::size_t offset = 0;
+
+            if (N > 2) {
+                bool starts_with_zero = arr[0] == '0';
+                bool is_hex = starts_with_zero && arr[1] == 'x';
+                bool is_binary = starts_with_zero && arr[1] == 'b';
+
+                if (is_hex) {
+                    //0xDEADBEEF (hexadecimal)
+                    base = 16;
+                    offset = 2;
+                }
+                else if (is_binary) {
+                    //0b101011101 (binary)
+                    base = 2;
+                    offset = 2;
+                }
+                else if (starts_with_zero) {
+                    //012345 (octal)
+                    base = 8;
+                    offset = 1;
+                }
+            }
+
+            long long number = 0;
+            long long multiplier = 1;
+
+            for (std::size_t i = 0; i < N - offset; ++i) {
+                char c = arr[N - 1 - i];
+                number += to_int(c) * multiplier;
+                multiplier *= base;
+            }
+
             return number;
         }
     }
