2ca3f76e3f7a40a430febe39ced318edbee5b954
Louis Dionne <ldionne.2@gmail.com>
2015-12-16 14:24:40 -0500

    Remove workarounds for Boost 1.58

--- test/ext/boost/fusion/list/auto/unique.cpp
+++ test/ext/boost/fusion/list/auto/unique.cpp
@@ -4,12 +4,7 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-#include <boost/version.hpp>
-
-// Appears to be broken on Boost 1.58.0
-#if BOOST_VERSION != 105800
-#   include "_specs.hpp"
-#   include "../../../../../auto/unique.hpp"
-#endif
+#include "_specs.hpp"
+#include "../../../../../auto/unique.hpp"
 
 int main() { }
