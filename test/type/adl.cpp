a4ec52dac0e230f33270365719afb5ef49c14fab
Louis Dionne <ldionne.2@gmail.com>
2016-01-04 10:59:30 -0500

    [type] Modularize the unit tests

--- test/type/adl.cpp
+++ test/type/adl.cpp
@@ -4,25 +4,23 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
 
-#include <boost/hana.hpp>
-
 #include <boost/hana/type.hpp>
-using namespace boost::hana;
+namespace hana = boost::hana;
 
 
 template <bool b = false>
 struct invalid { static_assert(b, "invalid must not be instantiated"); };
 
 template <typename T> void adl(T) { }
-template <typename T> void adl_pattern(basic_type<T>) { }
+template <typename T> void adl_pattern(hana::basic_type<T>) { }
 
 
 int main() {
     // ADL kicks in but `invalid<>` must not instantiated
-    adl(type_c<invalid<>>);
-    adl_pattern(type_c<invalid<>>);
+    adl(hana::type_c<invalid<>>);
+    adl_pattern(hana::type_c<invalid<>>);
 
     // ADL instantiates the types recursively, make sure that works too
-    adl(decltype_(type_c<invalid<>>));
-    adl_pattern(decltype_(type_c<invalid<>>));
+    adl(hana::decltype_(hana::type_c<invalid<>>));
+    adl_pattern(hana::decltype_(hana::type_c<invalid<>>));
 }
