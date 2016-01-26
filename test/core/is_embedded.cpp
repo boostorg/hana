aae8559d8aff9693ee3a1b886b229c365718f4b2
Louis Dionne <ldionne.2@gmail.com>
2016-01-25 22:11:12 -0500

    [test] Add unit tests for is_embedded

---
+++ test/core/is_embedded.cpp
@@ -0,0 +1,38 @@
+/*
+@copyright Louis Dionne 2013-2016
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+ */
+
+#include <boost/hana/core/to.hpp>
+namespace hana = boost::hana;
+
+
+// This test makes sure that fundamental types are properly embedded in
+// each other, when sensible.
+
+static_assert(hana::is_embedded<double, long double>{}, "");
+static_assert(hana::is_embedded<float, long double>{}, "");
+static_assert(hana::is_embedded<float, double>{}, "");
+
+static_assert(hana::is_embedded<long,        long long>{}, "");
+static_assert(hana::is_embedded<int,         long long>{}, "");
+static_assert(hana::is_embedded<short,       long long>{}, "");
+static_assert(hana::is_embedded<signed char, long long>{}, "");
+static_assert(hana::is_embedded<int,         long>{}, "");
+static_assert(hana::is_embedded<short,       long>{}, "");
+static_assert(hana::is_embedded<signed char, long>{}, "");
+static_assert(hana::is_embedded<short,       int>{}, "");
+static_assert(hana::is_embedded<signed char, int>{}, "");
+
+static_assert(hana::is_embedded<unsigned long,  unsigned long long>{}, "");
+static_assert(hana::is_embedded<unsigned int,   unsigned long long>{}, "");
+static_assert(hana::is_embedded<unsigned short, unsigned long long>{}, "");
+static_assert(hana::is_embedded<unsigned char,  unsigned long long>{}, "");
+static_assert(hana::is_embedded<unsigned int,   unsigned long>{}, "");
+static_assert(hana::is_embedded<unsigned short, unsigned long>{}, "");
+static_assert(hana::is_embedded<unsigned char,  unsigned long>{}, "");
+static_assert(hana::is_embedded<unsigned short, unsigned int>{}, "");
+static_assert(hana::is_embedded<unsigned char,  unsigned int>{}, "");
+
+int main() { }
