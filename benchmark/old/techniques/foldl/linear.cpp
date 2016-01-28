117c5047bf8ce7d922534636c93ca08db9b8acd3
Louis Dionne <ldionne.2@gmail.com>
2016-01-28 12:34:07 -0500

    [benchmarks] Remove old benchmarks that are not used anymore

--- benchmark/old/techniques/foldl/linear.cpp
+++
@@ -1,7 +0,0 @@
-<% render('foldl/functions.rb') %>
-
-<% 0.upto(unroll).each do |n| %>
-  <%= unroll_linear(n, trailing: n == unroll) %>
-<% end %>
-
-<%= render('foldl/main.cpp') %>
--- benchmark/old/techniques/foldl/log.cpp
+++
@@ -1,5 +0,0 @@
-<% render('foldl/functions.rb') %>
-
-<%= unroll_log(unroll) %>
-
-<%= render('foldl/main.cpp') %>
