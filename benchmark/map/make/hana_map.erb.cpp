afd5ca1b6c23ee66c87cfabe394feeca5a8cc88d
Louis Dionne <ldionne.2@gmail.com>
2016-02-29 11:15:50 -0500

    [map] Use Comparable keys in the make_map benchmark

--- benchmark/map/make/hana_map.erb.cpp
+++ benchmark/map/make/hana_map.erb.cpp
@@ -2,6 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
+#include <boost/hana/fwd/equal.hpp>
 #include <boost/hana/fwd/hash.hpp>
 #include <boost/hana/map.hpp>
 #include <boost/hana/type.hpp>
@@ -14,6 +15,12 @@ template <int i>
 struct x { };
 
 namespace boost { namespace hana {
+    template <int i, int j>
+    struct equal_impl<x<i>, x<j>> {
+        static constexpr hana::bool_<i == j> apply(x<i> const&, x<j> const&)
+        { return {}; }
+    };
+
     template <int i>
     struct hash_impl<x<i>> {
         static constexpr hana::type<x<i>> apply(x<i> const&) { return {}; }
