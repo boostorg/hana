9082e9ff05a806009340c97083ccb137d72713ad
Louis Dionne <ldionne.2@gmail.com>
2016-03-20 10:32:23 -0400

    [version] Bump to 1.0.0

--- include/boost/hana/version.hpp
+++ include/boost/hana/version.hpp
@@ -20,11 +20,11 @@ Distributed under the Boost Software License, Version 1.0.
 
 //! @ingroup group-config
 //! Macro expanding to the major version of the library, i.e. the `x` in `x.y.z`.
-#define BOOST_HANA_MAJOR_VERSION 0
+#define BOOST_HANA_MAJOR_VERSION 1
 
 //! @ingroup group-config
 //! Macro expanding to the minor version of the library, i.e. the `y` in `x.y.z`.
-#define BOOST_HANA_MINOR_VERSION 8
+#define BOOST_HANA_MINOR_VERSION 0
 
 //! @ingroup group-config
 //! Macro expanding to the patch level of the library, i.e. the `z` in `x.y.z`.
