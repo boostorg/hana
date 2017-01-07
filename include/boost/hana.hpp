9895803ede4b52cc81b5166d4fba10d654758a2d
Louis Dionne <ldionne.2@gmail.com>
2017-01-07 14:14:18 -0800

    Bump copyright year to 2017

--- include/boost/hana.hpp
+++ include/boost/hana.hpp
@@ -3,7 +3,7 @@
 Includes all the library components except the adapters for external
 libraries.
 
-@copyright Louis Dionne 2013-2016
+@copyright Louis Dionne 2013-2017
 Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
  */
