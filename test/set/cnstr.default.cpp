620a2f681d93a2fa58bd2fb88b1f0fb6faa6b5af
Louis Dionne <ldionne.2@gmail.com>
2017-01-29 13:06:41 -0800

    [set] Fix broken default constructibility unit test
    For some reason, this test failed in Travis when I added it, but the Travis
    build still passed so I never noticed it.

--- test/set/cnstr.default.cpp
+++ test/set/cnstr.default.cpp
@@ -3,50 +3,75 @@
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
 #include <boost/hana/bool.hpp>
+#include <boost/hana/fwd/hash.hpp>
 #include <boost/hana/set.hpp>
+#include <boost/hana/type.hpp>
 
 #include <type_traits>
 namespace hana = boost::hana;
 
 
+template <int i>
 struct NoDefault {
     NoDefault() = delete;
     NoDefault(NoDefault const&) = default;
     constexpr explicit NoDefault(int) { }
 };
 
-auto operator==(NoDefault const&, NoDefault const&) { return hana::true_c; }
-auto operator!=(NoDefault const&, NoDefault const&) { return hana::false_c; }
+template <int i, int j>
+auto operator==(NoDefault<i> const&, NoDefault<j> const&) { return hana::bool_<i == j>{}; }
+template <int i, int j>
+auto operator!=(NoDefault<i> const&, NoDefault<j> const&) { return hana::bool_<i != j>{}; }
 
+template <int i>
 struct Default {
     Default() = default;
     Default(Default const&) = default;
     constexpr explicit Default(int) { }
 };
 
-auto operator==(Default const&, Default const&) { return hana::true_c; }
-auto operator!=(Default const&, Default const&) { return hana::false_c; }
+template <int i, int j>
+auto operator==(Default<i> const&, Default<j> const&) { return hana::bool_<i == j>{}; }
+template <int i, int j>
+auto operator!=(Default<i> const&, Default<j> const&) { return hana::bool_<i != j>{}; }
+
+namespace boost { namespace hana {
+    template <int i>
+    struct hash_impl<NoDefault<i>> {
+        static constexpr auto apply(NoDefault<i> const&)
+        { return hana::type_c<NoDefault<i>>; };
+    };
+
+    template <int i>
+    struct hash_impl<Default<i>> {
+        static constexpr auto apply(Default<i> const&)
+        { return hana::type_c<Default<i>>; };
+    };
+}}
 
 int main() {
     {
         auto set0 = hana::make_set();
         static_assert(std::is_default_constructible<decltype(set0)>{}, "");
 
-        auto set1 = hana::make_set(Default(1));
+        auto set1 = hana::make_set(Default<0>(int{}));
         static_assert(std::is_default_constructible<decltype(set1)>{}, "");
 
-        auto set2 = hana::make_set(Default(1), Default(2));
+        auto set2 = hana::make_set(Default<0>(int{}), Default<1>(int{}));
         static_assert(std::is_default_constructible<decltype(set2)>{}, "");
+
+        auto set3 = hana::make_set(Default<0>(int{}), NoDefault<1>(int{}), Default<2>(int{}));
+        static_assert(!std::is_default_constructible<decltype(set3)>{}, "");
     }
 
     {
-        auto set1 = hana::make_set(NoDefault(1));
+        auto set1 = hana::make_set(NoDefault<0>(int{}));
         static_assert(!std::is_default_constructible<decltype(set1)>{}, "");
 
-        auto set2 = hana::make_set(NoDefault(1), NoDefault(2));
+        auto set2 = hana::make_set(NoDefault<0>(int{}), NoDefault<1>(int{}));
         static_assert(!std::is_default_constructible<decltype(set2)>{}, "");
 
-        auto set3 = hana::make_set(NoDefault(1), NoDefault(2), NoDefault(3));
+        auto set3 = hana::make_set(NoDefault<0>(int{}), NoDefault<1>(int{}), NoDefault<2>(int{}));
         static_assert(!std::is_default_constructible<decltype(set3)>{}, "");
     }
 }
