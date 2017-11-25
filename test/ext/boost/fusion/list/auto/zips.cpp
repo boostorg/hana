80939d494933a1d92da97198b334db7f8f2f011c
Louis Dionne <ldionne.2@gmail.com>
2017-11-25 13:48:01 -0800

    [ext.boost.fusion] Unconditionally enable some Fusion adaptor tests
    Those tests were broken with older versions of Boost that we don't test
    against anymore. Furthermore, since we document that Boost and standalone
    Hana installations should not be mixed, there's no risk of this change
    breaking any existing code (that would be mixing standalone Hana with an
    old version of Boost).

--- test/ext/boost/fusion/list/auto/zips.cpp
+++ test/ext/boost/fusion/list/auto/zips.cpp
@@ -2,9 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
-#if 0
 #include "_specs.hpp"
-#include <auto/permutations.hpp>
-#endif
+#include <auto/zips.hpp>
 
 int main() { }
